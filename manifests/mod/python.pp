puppetlabs/puppetlabs-apache

commit 2b79e8bdd11718d488912dbefcbd584144a1716a

(#11070) Add support for Scientific Linux
This patch adds Scientific Linux to the various case statements so it is
supported. This also fixed the various templates so that the log location
is variable depending on weither the OS is Redhat based or Debian based.

manifests/mod/python.pp
@@ -3,7 +3,7 @@ class apache::mod::python {
 
   package { "python":
     name => $operatingsystem ? {
-      centos  => "mod_python",
+      'centos', 'fedora', 'redhat', 'scientific' => "mod_python",
       default => "libapache2-mod-python",
     },
     ensure  => installed,

manifests/mod/wsgi.pp
@@ -3,7 +3,7 @@ class apache::mod::wsgi {
 
   package { "wsgi":
     name => $operatingsystem ? {
-      centos  => "mod_wsgi",
+      'centos', 'fedora', 'redhat', 'scientific' => "mod_wsgi",
       default => "libapache2-mod-wsgi",
     },
     ensure  => installed,

manifests/params.pp
@@ -31,7 +31,7 @@ class apache::params {
   $vhost_name    = '*'
 
   case $operatingsystem {
-    'centos', 'redhat', 'fedora': {
+    'centos', 'redhat', 'fedora', 'scientific': {
        $apache_name = 'httpd'
        $php_package = 'php'
        $ssl_package = 'mod_ssl'

manifests/ssl.pp
@@ -17,7 +17,7 @@ class apache::ssl {
   include apache
   
   case $operatingsystem {
-     'centos', 'fedora', 'redhat': {
+     'centos', 'fedora', 'redhat', 'scientific': {
         package { $apache::params::ssl_package:
            require => Package['httpd'],
         }

manifests/vhost.pp
@@ -36,6 +36,7 @@ define apache::vhost(
     $auth          = $apache::params::auth,
     $redirect_ssl  = $apache::params::redirect_ssl,
     $options       = $apache::params::options,
+    $apache_name   = $apache::params::apache_name,
     $vhost_name    = $apache::params::vhost_name
   ) {
 

templates/vhost-default.conf.erb
@@ -18,9 +18,9 @@ NameVirtualHost <%= vhost_name %>:<%= port %>
     Order allow,deny
     allow from all
   </Directory>
-  ErrorLog /var/log/apache2/<%= name %>_error.log
+  ErrorLog /var/log/<%= apache_name %>/<%= name %>_error.log
   LogLevel warn
-  CustomLog /var/log/apache2/<%= name %>_access.log combined
+  CustomLog /var/log/<%= apache_name %>/<%= name %>_access.log combined
   ServerSignature Off
 </VirtualHost>
 

templates/vhost-proxy.conf.erb
@@ -20,9 +20,9 @@ NameVirtualHost <%= vhost_name %>:<%= port %>
    ProxyPassReverse / <%= dest %>/
    ProxyPreserveHost On 
 
-   ErrorLog /var/log/apache2/<%= name %>_error.log
+   ErrorLog /var/log/<%= apache_name %>/<%= name %>_error.log
    LogLevel warn
-   CustomLog /var/log/apache2/<%= name %>_access.log combined
+   CustomLog /var/log/<%= apache_name %>/<%= name %>_access.log combined
 
 </VirtualHost>
 

commit 976cb69648d9b8b9e454903774046e4d0a164aeb

(#13072) Move mod python and wsgi package names to params
Previously apache::mod::python and apache::mod::wsgi used
a case statement to determine which package to install. This
commit moves the package names to the params class and fixes
the syntax errors introduced in commit 2b79e8b when the
selectors were changed to include Scientific Linux.

manifests/mod/python.pp
@@ -1,11 +1,8 @@
 class apache::mod::python {
   include apache
 
-  package { "python":
-    name => $operatingsystem ? {
-      'centos', 'fedora', 'redhat', 'scientific' => "mod_python",
-      default => "libapache2-mod-python",
-    },
+  package { "mod_python_package":
+    name    => $apache::params::mod_python_package,
     ensure  => installed,
     require => Package["httpd"];
   }

manifests/mod/wsgi.pp
@@ -1,11 +1,8 @@
 class apache::mod::wsgi {
   include apache
 
-  package { "wsgi":
-    name => $operatingsystem ? {
-      'centos', 'fedora', 'redhat', 'scientific' => "mod_wsgi",
-      default => "libapache2-mod-wsgi",
-    },
+  package { "mod_wsgi_package":
+    name    => $apache::params::mod_wsgi_package,
     ensure  => installed,
     require => Package["httpd"];
   }

manifests/params.pp
@@ -34,6 +34,8 @@ class apache::params {
     'centos', 'redhat', 'fedora', 'scientific': {
        $apache_name = 'httpd'
        $php_package = 'php'
+       $mod_python_package = 'mod_python'
+       $mod_wsgi_package = 'mod_wsgi'
        $ssl_package = 'mod_ssl'
        $apache_dev  = 'httpd-devel'
        $vdir = '/etc/httpd/conf.d/'
@@ -41,6 +43,8 @@ class apache::params {
     'ubuntu', 'debian': {
        $apache_name = 'apache2'
        $php_package = 'libapache2-mod-php5'
+       $mod_python_package = 'libapache2-mod-python'
+       $mod_wsgi_package = 'libapache2-mod-wsgi'
        $ssl_package = 'apache-ssl'
        $apache_dev  = [ 'libaprutil1-dev', 'libapr1-dev', 'apache2-prefork-dev' ]
        $vdir = '/etc/apache2/sites-enabled/'
@@ -48,6 +52,8 @@ class apache::params {
     default: {
        $apache_name = 'apache2'
        $php_package = 'libapache2-mod-php5'
+       $mod_python_package = 'libapache2-mod-python'
+       $mod_wsgi_package = 'libapache2-mod-wsgi'
        $ssl_package = 'apache-ssl'
        $apache_dev  = 'apache-dev'
        $vdir = '/etc/apache2/sites-enabled/'