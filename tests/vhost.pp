2c1f07202bef8427dfb3dd604c5ac52243c41d66
Ryan Coleman <ryan@puppetlabs.com>
2012-04-20 08:38:49 -0700

    Update smoke tests
    This commit fixes up the vhost smoke tests after a rebase to include
    four examples, one of which uses the vhost erb template.

--- tests/vhost.pp
+++ tests/vhost.pp
@@ -1,6 +1,27 @@
 include apache
+
 apache::vhost { 'test.vhost':
   port     => 80,
   docroot  => '/tmp/testvhost',
   template => 'apache/test.vhost.erb'
+}
+
+# Example smoke test of multiple vhosts
+# and multiple listening ports
+apache::vhost { 'port.80':
+  priority => '20',
+  port => '80',
+  docroot => '/var/www',
+}
+
+apache::vhost { 'port.443':
+  priority => '21',
+  port => '443',
+  docroot => '/var/www',
+}
+
+apache::vhost { 'second.port.443':
+  priority => '22',
+  port => '443',
+  docroot => '/var/www',
 }
