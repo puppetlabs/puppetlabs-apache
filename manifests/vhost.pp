c7196ff53358739d812e2a9ba92dd4d5b8b43910
Anand Kumria <akumria@acm.org>
2012-06-16 20:43:05 +0100

    Make logroot a parameter than can be overriden in the vhost definition.
    Again, this should no impact to existing users but will allow those who
    want to customise the directory where the logs go to do so.
    
    And it allows me to bring my fork closer to the standard module.

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -41,7 +41,8 @@ define apache::vhost(
     $redirect_ssl       = $apache::params::redirect_ssl,
     $options            = $apache::params::options,
     $apache_name        = $apache::params::apache_name,
-    $vhost_name         = $apache::params::vhost_name
+    $vhost_name         = $apache::params::vhost_name,
+    $logroot            = "/var/log/" + $apache::params::apache_name
   ) {
 
   include apache
