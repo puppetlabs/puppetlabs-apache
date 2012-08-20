b1566b6bd816408815ba288fba60167d6fe8566c
Hunter Haugen <h.haugen@gmail.com>
2012-08-19 21:08:04 -0700

    Change `$module` to `$mod` to cope with puppet-rspec bugs

--- manifests/mod.pp
+++ manifests/mod.pp
@@ -1,28 +1,29 @@
 define apache::mod (
   $package = undef
 ) {
-  $module = $name
-  include apache
+  $mod = $name
+  include apache::params
+  #include apache #This creates duplicate resources in rspec-puppet
   $mod_packages = $apache::params::mod_packages
   if $package {
     $package_REAL = $package
-  } elsif $mod_packages[$module] {
-    $package_REAL = $mod_packages[$module]
+  } elsif $mod_packages[$mod] {
+    $package_REAL = $mod_packages[$mod]
   }
   $mod_libs = $apache::params::mod_libs
   if $mod_libs {
-    $lib = $mod_libs[$module]
+    $lib = $mod_libs[$mod]
   }
 
   if $package_REAL {
     package { $package_REAL:
       ensure  => present,
       require => Package['httpd'],
-      before  => A2mod[$module],
+      before  => A2mod[$mod],
     }
   }
 
-  a2mod { $module:
+  a2mod { $mod:
     ensure  => present,
     lib     => $lib,
     require => Package['httpd'],
