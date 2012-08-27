cc81ed9dd2c082c445209997e6bc024f96311d3a
Ryan Coleman <ryan@puppetlabs.com>
2012-08-27 12:55:33 -0700

    (#16134) Quote class names inside relationships
    Prior to this commit, the chaining resource relationship syntax
    used in several of the apache::mod::* manifests specified class
    relationships without quoting the class names. Apparently, this
    broke the parser in Puppet 2.6.x. This commit wraps those class
    names inside single quotes, making the 2.6 & 2.7 parser happy.

--- manifests/mod/disk_cache.pp
+++ manifests/mod/disk_cache.pp
@@ -1,8 +1,8 @@
 class apache::mod::disk_cache (
   $cache_root = '/var/cache/mod_proxy'
 ) {
-  Class[apache::mod::proxy] -> Class[apache::mod::disk_cache]
-  Class[apache::mod::cache] -> Class[apache::mod::disk_cache]
+  Class['apache::mod::proxy'] -> Class['apache::mod::disk_cache']
+  Class['apache::mod::cache'] -> Class['apache::mod::disk_cache']
 
   apache::mod { 'disk_cache': }
   # Template uses $cache_proxy
