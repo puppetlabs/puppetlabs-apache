d6027771cf5dec7e80e93c37c5542d4f48e179a9
Aaron Hicks <Aethylred@gmail.com>
2012-09-21 11:07:35 +1200

    use Augeas to put in the correct domain name

--- manifests/mod/shib.pp
+++ manifests/mod/shib.pp
@@ -1,9 +1,11 @@
 class apache::mod::shib(
 	$shib_admin			= $apache::serveradmin,
+	$shib_hostname	= $fqdn,
 	$logoLocation		= "/shibboleth-sp/logo.jpg",
 	$styleSheet			= "/shibboleth-sp/main.css",
 	$shib_conf_dir	= '/etc/shibboleth',
-	$shib_conf_file	= 'shibboleth2.xml'
+	$shib_conf_file	= 'shibboleth2.xml',
+	$handler_ssl		= true
 ){
 
 	$shib_conf = "${shib_conf_dir}/${shib_conf_file}"
@@ -30,7 +32,21 @@ class apache::mod::shib(
 			"set Errors/#attribute/supportContact ${shib_admin}",
 			"set Errors/#attribute/logoLocation ${logoLocation}",
 			"set Errors/#attribute/styleSheet ${styleSheet}",
-		]
+		],
+		notify	=> Service['httpd'],
+	}
+
+	# handlerSSL probably shouldn't be in this augeas block...
+	augeas{"shib_SPconfig_hostname":
+		lens		=> 'Xml.lns',
+		incl		=> $shib_conf,
+		context => "/files${shib_conf}/SPConfig/ApplicationDefaults",
+		changes => [
+			"set #attribute/entityID https://${shib_hostname}/shibboleth",
+			"set Sessions/#attribute/handlerURL https://${shib_hostname}/Shibboleth.sso",
+			"set Sessions/#attribute/handlerSSL ${handlerSSL}",
+		],
+		notify	=> Service['httpd'],
 	}
 
 }
