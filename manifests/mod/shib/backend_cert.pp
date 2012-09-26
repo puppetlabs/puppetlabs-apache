5db41b168d31fe5cd9f1cab607df6b68adb7e93f
Aaron Hicks <Aethylred@gmail.com>
2012-09-27 11:57:14 +1200

    more directories are reuqired in the exec path

--- manifests/mod/shib/backend_cert.pp
+++ manifests/mod/shib/backend_cert.pp
@@ -7,7 +7,7 @@ class apache::mod::shib::backend_cert(
 	# The test should not be that the file exists, but that the file has
 	# the correct hostname in it
 	exec{"shib_keygen_${hostname}":
-		path 		=> $apache::mod::shib::shib_bin_dir,
+		path 		=> [$apache::mod::shib::shib_bin_dir,'/usr/bin','/bin'],
 		command	=> "shib-keygen -h ${hostname} -e https://${hostname}/shibbloeth",
 		creates => "${apache::mod::shib::shib_conf_dir}/${apache::mod::shib::shib_sp_cert}",
 	}
