c47f12b984b0ca282b40653190e2b8beed567622
Andreas Ntaflos <daff@pseudoterminal.org>
2012-11-11 02:27:41 +0100

    Use defined type apache::mod for installing and enabling passenger.

--- manifests/mod/passenger.pp
+++ manifests/mod/passenger.pp
@@ -1,16 +1,5 @@
 class apache::mod::passenger {
   include 'apache'
-  include 'apache::params'
 
-  package { 'mod_passenger_package':
-    ensure  => installed,
-    name    => $apache::params::mod_passenger_package,
-    require => Package['httpd'],
-  }
-
-  a2mod { 'passenger':
-    ensure  => present,
-    require => Package['mod_passenger_package'],
-    notify  => Service['httpd'],
-  }
+  apache::mod { 'passenger': }
 }
