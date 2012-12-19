15bb9a067e57dde3a9184ddf65daa1bd1cb7c872
Hunter Haugen <h.haugen@gmail.com>
2012-12-19 10:29:37 -0800

    Add a2mod instances method on Debian
    The a2mod Debian provider (just called `a2mod.rb` instead of
    `debian.rb`) did not have an instances method which did not allow
    resource purging or `puppet resource a2mod` to work. This commit adds
    this by using the `apache2ctl -M` command which lists loaded modules.

--- lib/puppet/provider/a2mod/a2mod.rb
+++ lib/puppet/provider/a2mod/a2mod.rb
@@ -3,9 +3,25 @@ Puppet::Type.type(:a2mod).provide(:a2mod) do
 
     optional_commands :encmd => "a2enmod"
     optional_commands :discmd => "a2dismod"
+    optional_commands :apache2ctl => "apache2ctl"
 
     confine :osfamily => :debian
     defaultfor :operatingsystem => [:debian, :ubuntu]
+
+    def self.instances
+      modules = apache2ctl("-M").collect { |line|
+        m = line.match(/(\w+)_module \(shared\)$/)
+        m[1] if m
+      }.compact
+
+      modules.map do |mod|
+        new(
+          :name     => mod,
+          :ensure   => :present,
+          :provider => :a2mod
+        )
+      end
+    end
 
     def create
         encmd resource[:name]
@@ -16,7 +32,6 @@ Puppet::Type.type(:a2mod).provide(:a2mod) do
     end
 
     def exists?
-        mod= "/etc/apache2/mods-enabled/" + resource[:name] + ".load"
-        File.exists?(mod)
+      @property_hash[:ensure] != :absent
     end
 end
