205872787be272287ec518e4a58e0117ddfa1afb
Hunter Haugen <h.haugen@gmail.com>
2013-02-21 09:40:01 -0800

    Add spec tests for vhost/confd/mod dirs

--- spec/classes/apache_spec.rb
+++ spec/classes/apache_spec.rb
@@ -50,5 +50,41 @@ describe 'apache', :type => :class do
       'require' => 'Package[httpd]'
       )
     }
+    describe "Alternate confd/mod/vhosts directory" do
+      let :params do
+        {
+          :vhost_dir => '/etc/httpd/conf.d',
+          :confd_dir => '/etc/httpd/conf.d',
+          :mod_dir   => '/etc/httpd/conf.d',
+        }
+      end
+
+      it { should contain_file("/etc/httpd/conf.d").with(
+        'ensure'  => 'directory',
+        'recurse' => 'true',
+        'purge'   => 'true',
+        'notify'  => 'Service[httpd]',
+        'require' => 'Package[httpd]'
+      ) }
+      it { should_not contain_file("/etc/httpd/site.d") }
+      it { should_not contain_file("/etc/httpd/mod.d") }
+      it { should contain_file("/etc/httpd/conf/httpd.conf").with_content %r{^Include /etc/httpd/conf\.d/\*\.conf$} }
+      it { should contain_file("/etc/httpd/conf/httpd.conf").with_content %r{^Include /etc/httpd/conf\.d/\*\.load$} }
+    end
+
+    describe "Alternate conf.d directory" do
+      let :params do
+        { :confd_dir => '/etc/httpd/special_conf.d' }
+      end
+
+      it { should contain_file("/etc/httpd/special_conf.d").with(
+        'ensure'  => 'directory',
+        'recurse' => 'true',
+        'purge'   => 'true',
+        'notify'  => 'Service[httpd]',
+        'require' => 'Package[httpd]'
+      ) }
+      it { should_not contain_file("/etc/httpd/conf.d") }
+    end
   end
 end
