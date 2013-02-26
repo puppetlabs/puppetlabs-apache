d9eb365fb4e8325633a8f0468515c6d794562ee4
Hunter Haugen <h.haugen@gmail.com>
2013-02-26 09:41:55 -0800

    Add priority to default ssl

--- manifests/init.pp
+++ manifests/init.pp
@@ -146,6 +146,7 @@ class apache (
         scriptalias     => $scriptalias,
         serveradmin     => $serveradmin,
         access_log_file => "ssl_${access_log_file}",
+        priority        => '15',
       }
     }
   }
