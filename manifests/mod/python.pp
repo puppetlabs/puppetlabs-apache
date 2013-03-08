5f136a9b47f8ed9f12536d244d4cd09f2309e658
vgomesgo <vgomesgo@VGOMESGOPC.cern.ch>
2013-03-08 15:40:43 +0100

    Fix for the apache::mod::python module

--- manifests/mod/python.pp
+++ manifests/mod/python.pp
@@ -1,6 +1,10 @@
 class apache::mod::python {
   #include apache
   apache::mod { 'python': }
+  include apache::params
+  file { "python.conf":
+    path => "${apache::params::vdir}/python.conf",
+  }
 }
 
 
