db9d5f69a4a75b27c06a7342ec66c26ad5ed83b2
Thomas Linkin <trlinkin@gmail.com>
2013-03-21 16:35:24 -0400

    Control server-info location This commit enhances the control over the server-info location. It includes the ability to enable/disable it, as well as controll the allow list.

--- manifests/mod/info.pp
+++ manifests/mod/info.pp
@@ -1,9 +1,14 @@
-class apache::mod::info {
+class apache::mod::info (
+  $enable     = false,
+  $allow_from = ['127.0.0.1','::1'],
+){
   apache::mod { 'info': }
-  # Template uses no variables
-  file { 'info.conf':
-    ensure  => present,
-    path    => "${apache::mod_dir}/info.conf",
-    content => template('apache/mod/info.conf.erb'),
+  # Template uses $allow_from
+  if $enable {
+    file { 'info.conf':
+      ensure  => present,
+      path    => "${apache::mod_dir}/info.conf",
+      content => template('apache/mod/info.conf.erb'),
+    }
   }
 }
