35933aabb06f8204f1c714292dab87adff5fc6e7
Brandon Schlueter <brandon@elephantventures.com>
2013-03-29 17:58:19 -0400

    Add require(d) directive w/ auth_dir Directory
    Conflicts:
    
    	manifests/vhost.pp
    	templates/vhost-default.conf.erb

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -56,7 +56,22 @@ define apache::vhost(
     $vhost_name         = $apache::params::vhost_name,
     $logroot            = "/var/log/$apache::params::apache_name",
     $access_log         = true,
-    $ensure             = 'present'
+    $ensure             = 'present',
+    $wsgi_python_home   = false,
+    $wsgi_script_url    = '/',
+    $wsgi_script        = false,
+    $alias_url          = '/',
+    $alias_target       = false,
+    # alias_dir_options is a hash of directive => param settings for the alias Directory directive
+    $alias_dir_options  = {},
+    $auth_dir           = false,
+    $required            = false,
+    $auth_type          = "Basic",
+    $auth_name          = "Project",
+    $auth_user_file     = false,
+    $auth_group_file    = false,
+    $userids            = false,
+    $groups             = false,
   ) {
 
   validate_re($ensure, '^(present|absent)$',
