4c3c885afe2c1a9b9793040a01d41fba19d892ec
Thomas Linkin <trlinkin@gmail.com>
2013-04-23 17:00:37 -0400

    Remove enable parameter from apache::mod::info
    Originally, apache::mod::info was designed with the ability to be
    included but disabled. This was a misinterpretation of default apache
    behavior. Ultimately, it is not required to have an enabled option.
    
    This patch removes the 'enable' parameter from apache::mod::info. It will
    now be dependent on the declaration of the class as to wether or not
    mod_info is active.

--- manifests/mod/info.pp
+++ manifests/mod/info.pp
@@ -1,14 +1,11 @@
 class apache::mod::info (
-  $enable     = false,
   $allow_from = ['127.0.0.1','::1'],
 ){
   apache::mod { 'info': }
   # Template uses $allow_from
-  if $enable {
-    file { 'info.conf':
-      ensure  => present,
-      path    => "${apache::mod_dir}/info.conf",
-      content => template('apache/mod/info.conf.erb'),
-    }
+  file { 'info.conf':
+    ensure  => present,
+    path    => "${apache::mod_dir}/info.conf",
+    content => template('apache/mod/info.conf.erb'),
   }
 }
