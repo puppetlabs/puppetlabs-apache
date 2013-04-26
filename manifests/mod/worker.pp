2f99132d16a5b971da908956466a52de736c8940
Hunter Haugen <h.haugen@gmail.com>
2013-04-25 17:55:51 -0700

    Updating apache::mod::prefork and adding apache::mod::worker
    Also adding debian support

--- manifests/mod/worker.pp
+++ manifests/mod/worker.pp
@@ -1,17 +1,31 @@
 class apache::mod::worker (
-  $serverlimit         = '25',
   $startservers        = '2',
   $maxclients          = '150',
   $minsparethreads     = '25',
   $maxsparethreads     = '75',
   $threadsperchild     = '25',
   $maxrequestsperchild = '0',
-){
+  $serverlimit         = '25',
+) {
+  if defined(Class['apache::mod::prefork']) {
+    fail('May not include both apache::mod::worker and apache::mod::prefork on the same node')
+  }
+  File {
+    owner => 'root',
+    group => 'root',
+    mode  => '0644',
+  }
 
-  file { "${apache::params::mod_dir}/worker.conf":
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0755',
+  # Template uses:
+  # - $startservers
+  # - $maxclients
+  # - $minsparethreads
+  # - $maxsparethreads
+  # - $threadsperchild
+  # - $maxrequestsperchild
+  # - $serverlimit
+  file { "${apache::mod_dir}/worker.conf":
+    ensure  => file,
     content => template('apache/mod/worker.conf.erb'),
   }
 
@@ -22,6 +36,16 @@ class apache::mod::worker (
         path   => '/etc/sysconfig/httpd',
         line   => 'HTTPD=/usr/sbin/httpd.worker',
         match  => '#?HTTPD=',
+        notify => Service['httpd'],
+      }
+    }
+    'debian': {
+      file { "${apache::mod_enable_dir}/worker.conf":
+        ensure => link,
+        target => "${apache::mod_dir}/worker.conf",
+      }
+      package { 'apache2-mpm-worker':
+        ensure => present,
       }
     }
   }
