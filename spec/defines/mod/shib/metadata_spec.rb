a0d88082bcddea0706eb6813aa9b0a04d8d9a0a0
Aaron Hicks <hicksa@landcareresearch.co.nz>
2013-05-03 09:22:49 +1200

    added basic spec test for metadata resource definition

---
+++ spec/defines/mod/shib/metadata_spec.rb
@@ -0,0 +1,37 @@
+describe 'apache::mod::shib::metadata', :type => :define do
+  let :pre_condition do
+    'include apache'
+  end
+  context "on a Debian OS" do
+    let :facts do
+      {
+        :osfamily               => 'Debian',
+        :operatingsystemrelease => '6',
+        :concat_basedir         => '/dne',
+      }
+    end
+    let(:title){ 'metadata_name' }
+    let(:params){ { :attribute_map_uri => 'http://example.org/attribute_map.xml' } }
+    it { should include_class("apache::params") }
+    it { should include_class("apache::mod::shib") }
+    it { should contain_exec("get_metadata_name_metadata_cert")}
+    it { should contain_augeas("shib_metadata_name_create_metadata_provider") }
+    it { should contain_augeas("shib_metadata_name_metadata_provider") }
+  end
+  context "on a RedHat OS" do
+    let :facts do
+      {
+        :osfamily               => 'RedHat',
+        :operatingsystemrelease => '6',
+        :concat_basedir         => '/dne',
+      }
+    end
+    let(:title){ 'metadata_name' }
+    let(:params){ { :attribute_map_uri => 'http://example.org/attribute_map.xml' } }
+    it { should include_class("apache::params") }
+    it { should include_class("apache::mod::shib") }
+    it { should contain_exec("get_metadata_name_metadata_cert") }
+    it { should contain_augeas("shib_metadata_name_create_metadata_provider") }
+    it { should contain_augeas("shib_metadata_name_metadata_provider") }
+  end
+end
