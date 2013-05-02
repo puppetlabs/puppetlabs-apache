2c7220aa05f57d8226d5f3ba7bf47ae9df385991
Aaron Hicks <hicksa@landcareresearch.co.nz>
2013-05-02 16:19:31 +1200

    ah, have to pass the sp_hostname as a parameter

--- spec/classes/mod/shib/backend_cert_spec.rb
+++ spec/classes/mod/shib/backend_cert_spec.rb
@@ -1,7 +1,6 @@
 describe 'apache::mod::shib::backend_cert', :type => :class do
   let :pre_condition do
     'include apache'
-    'include apache::mod::shib'
   end
   context "on a Debian OS" do
     let :facts do
@@ -9,9 +8,9 @@ describe 'apache::mod::shib::backend_cert', :type => :class do
         :osfamily               => 'Debian',
         :operatingsystemrelease => '6',
         :concat_basedir         => '/dne',
-        :fqdn                   => 'test.example.org',
       }
     end
+    let(:params){ { :sp_hostname => 'test.example.org' } }
     it { should include_class("apache::params") }
     it { should include_class("apache::mod::shib") }
     it { should contain_exec("shib_keygen_test.example.org")}
@@ -22,9 +21,9 @@ describe 'apache::mod::shib::backend_cert', :type => :class do
         :osfamily               => 'RedHat',
         :operatingsystemrelease => '6',
         :concat_basedir         => '/dne',
-        :fqdn                   => 'test.example.org',
       }
     end
+    let(:params){ { :sp_hostname => 'test.example.org' } }
     it { should include_class("apache::params") }
     it { should include_class("apache::mod::shib") }
     it { should contain_exec("shib_keygen_test.example.org")}
