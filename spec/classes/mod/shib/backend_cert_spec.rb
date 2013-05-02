1277654d742ab5391d550552b6d7d9d90bc8c50d
Aaron Hicks <hicksa@landcareresearch.co.nz>
2013-05-02 15:24:48 +1200

    spec test for backend_cert

---
+++ spec/classes/mod/shib/backend_cert_spec.rb
@@ -0,0 +1,29 @@
+describe 'apache::mod::shib', :type => :class do
+  let :pre_condition do
+    'include apache'
+  end
+  context "on a Debian OS" do
+    let :facts do
+      {
+        :osfamily               => 'Debian',
+        :operatingsystemrelease => '6',
+        :concat_basedir         => '/dne',
+        :fqdn                   => 'test.example.org',
+      }
+    end
+    it { should include_class("apache::params") }
+    it { should contain_exec("shib_keygen_test.example.org")}
+  end
+  context "on a RedHat OS" do
+    let :facts do
+      {
+        :osfamily               => 'RedHat',
+        :operatingsystemrelease => '6',
+        :concat_basedir         => '/dne',
+        :fqdn                   => 'test.example.org',
+      }
+    end
+    it { should include_class("apache::params") }
+    it { should contain_exec("shib_keygen_test.example.org")}
+  end
+end
