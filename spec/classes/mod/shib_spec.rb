97932c0e5435511976421e6e46dbc13bcdf30580
Aaron Hicks <hicksa@landcareresearch.co.nz>
2013-05-02 15:13:11 +1200

    Correct typo in spec test.

--- spec/classes/mod/shib_spec.rb
+++ spec/classes/mod/shib_spec.rb
@@ -12,7 +12,7 @@ describe 'apache::mod::shib', :type => :class do
     end
     it { should include_class("apache::params") }
     it { should contain_apache__mod('shib2') }
-    it { shoucl contain_package('libapache2-mod-shib2')}
+    it { should contain_package('libapache2-mod-shib2')}
     it { should contain_file("/etc/shibboleth") }
     it { should contain_file("/etc/shibboleth/shibboleth2.xml") }
     it { should contain_augeas("shib_SPconfig_errors") }
