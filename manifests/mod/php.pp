89aed1d68bd9a145407ca814d02a81c40c2b5538
Aaron Hicks <hicksa@landcareresearch.co.nz>
2013-05-16 12:51:08 +1200

    remove require call, and use metaparameter instead

--- manifests/mod/php.pp
+++ manifests/mod/php.pp
@@ -1,9 +1,9 @@
 class apache::mod::php {
-  require apache::mod::prefork
   apache::mod { 'php5': }
   file { 'php.conf':
     ensure  => file,
     path    => "${apache::mod_dir}/php.conf",
     content => template('apache/mod/php.conf.erb'),
+    require => Class['apache::mod::prefork'],
   }
 }
