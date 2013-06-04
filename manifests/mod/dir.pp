f2c618e69bb931b6cfe8bab35600b83b2e184046
Aaron Hicks <hicksa@landcareresearch.co.nz>
2013-06-05 11:59:56 +1200

    This allows an override of the default DirectoryIndex directive in dir.conf.erb

--- manifests/mod/dir.pp
+++ manifests/mod/dir.pp
@@ -1,9 +1,14 @@
+# Note: this sets the global DirectoryIndex directive, it may be necessary to consider being able to modify the apache::vhost to declare DirectoryIndex statements in a vhost configuration
+# Parameters:
+# - $indexes provides a string for the DirectoryIndex directive http://httpd.apache.org/docs/current/mod/mod_dir.html#directoryindex
 class apache::mod::dir (
-  $dir = 'public_html',
+  $dir     = 'public_html',
+  $indexes = undef,
 ) {
   apache::mod { 'dir': }
 
-  # Template uses no variables
+  # Template uses
+  # - $indexes
   file { 'dir.conf':
     ensure  => file,
     path    => "${apache::mod_dir}/dir.conf",
