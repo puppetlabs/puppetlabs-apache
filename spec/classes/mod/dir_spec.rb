5fc0c0d490aea8a99fd8437a968d3e26b4bd6f97
Aaron Hicks <hicksa@landcareresearch.co.nz>
2013-06-06 12:05:29 +1200

    Add basic spec test for mod_dir
    Note: No package test as it seems mod_dir is distributed _with_ the Apache package.

---
+++ spec/classes/mod/dir_spec.rb
@@ -0,0 +1,27 @@
+describe 'apache::mod::dir', :type => :class do
+  let :pre_condition do
+    'include apache'
+  end
+  context "on a Debian OS" do
+    let :facts do
+      {
+        :osfamily               => 'Debian',
+        :operatingsystemrelease => '6',
+        :concat_basedir         => '/dne',
+      }
+    end
+    it { should include_class("apache::params") }
+    it { should contain_apache__mod('dir') }
+  end
+  context "on a RedHat OS" do
+    let :facts do
+      {
+        :osfamily               => 'RedHat',
+        :operatingsystemrelease => '6',
+        :concat_basedir         => '/dne',
+      }
+    end
+    it { should include_class("apache::params") }
+    it { should contain_apache__mod('dir') }
+  end
+end
