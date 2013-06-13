acaa171b66ac8a2a6cd8a08a392ba6cea2c679e1
Hunter Haugen <h.haugen@gmail.com>
2013-06-13 14:36:24 -0700

    Adding python.conf for redhat systems

--- manifests/mod/python.pp
+++ manifests/mod/python.pp
@@ -1,5 +1,11 @@
 class apache::mod::python {
   apache::mod { 'python': }
+  if $::osfamily == 'RedHat' {
+    # Template uses no variables
+    file { 'python.conf':
+      ensure  => file,
+      path    => "${apache::mod_dir}/python.conf",
+      content => template('apache/mod/python.conf.erb'),
+    }
+  }
 }
-
-
