1051db22941e159215fcc2a8d23595e90726e33d
Stefan Dietrich <stefan.dietrich@desy.de>
2013-08-16 20:21:40 +0200

    Allow configuration of WSGISocketPrefix for mod_wsgi

--- manifests/mod/wsgi.pp
+++ manifests/mod/wsgi.pp
@@ -1,4 +1,18 @@
-class apache::mod::wsgi {
+class apache::mod::wsgi (
+  $wsgi_socket_prefix = undef
+){
   apache::mod { 'wsgi': }
+
+  if $wsgi_socket_prefix != undef {
+    # Template uses: $wsgi_socket_prefix
+    file {'wsgi.conf':
+      ensure  => file,
+      path    => "${apache::mod_dir}/wsgi.conf",
+      content => template('apache/mod/wsgi.conf.erb'),
+      require => Exec["mkdir ${apache::mod_dir}"],
+      before  => File[$apache::mod_dir],
+      notify  => Service['httpd']
+    }
+  }
 }
 
