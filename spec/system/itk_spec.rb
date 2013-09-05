3ee49eb076c3696f4bc95449135cb68b4a5debf0
Hunter Haugen <hunter@puppetlabs.com>
2013-09-05 14:33:25 -0700

    Revert "Merge pull request #349 from blkperl/fix_system_spec_on_rhel"
    This reverts commit 714777ef6dedbccca8ada86a80fba009a93aebd7, reversing
    changes made to 0b47f7ccc6a7d348ac29c92c0ea3b0a98a6cdd41.

--- spec/system/itk_spec.rb
+++ spec/system/itk_spec.rb
@@ -1,32 +1,33 @@
 require 'spec_helper_system'
 
-describe 'apache::mod::itk class' do
-  case node.facts['osfamily']
-  when 'Debian'
-    servicename = 'apache2'
-    describe 'running puppet code' do
-      # Using puppet_apply as a helper
-      it 'should work with no errors' do
-        pp = <<-EOS
-          class { 'apache':
-            mpm_module => 'itk',
-          }
-        EOS
+case node.facts['osfamily']
+when 'Debian'
+  servicename = 'apache2'
+else
+  raise Error, "Unconfigured OS for apache service on #{node.facts['osfamily']}"
+end
 
-        # Run it twice and test for idempotency
-        puppet_apply(pp) do |r|
-          r.exit_code.should_not == 1
-          r.refresh
-          r.exit_code.should be_zero
-        end
+describe 'apache::mod::itk class' do
+  describe 'running puppet code' do
+    # Using puppet_apply as a helper
+    it 'should work with no errors' do
+      pp = <<-EOS
+        class { 'apache':
+          mpm_module => 'itk',
+        }
+      EOS
+
+      # Run it twice and test for idempotency
+      puppet_apply(pp) do |r|
+        r.exit_code.should_not == 1
+        r.refresh
+        r.exit_code.should be_zero
       end
     end
+  end
 
-    describe service(servicename) do
-      it { should be_running }
-      it { should be_enabled }
-    end
-  when 'RedHat'
-    # Not implemented yet
+  describe service(servicename) do
+    it { should be_running }
+    it { should be_enabled }
   end
 end
