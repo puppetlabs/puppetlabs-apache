4027cdd1c246ea519631cfa3a0e4138f351e1f0c
John Bartko <jbartko@txstate.edu>
2013-09-08 18:15:52 -0500

    include ldap prereq

--- manifests/mod/authnz_ldap.pp
+++ manifests/mod/authnz_ldap.pp
@@ -1,6 +1,7 @@
 class apache::mod::authnz_ldap(
   $ldap_conf = 'apache/mod/authnz_ldap.conf.erb'
 ) {
+  include apache::mod::ldap
   apache::mod { 'authnz_ldap': }
   file { 'authnz_ldap.conf':
     ensure  => file,
