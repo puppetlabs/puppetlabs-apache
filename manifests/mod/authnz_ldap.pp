c2a7d5f2d6a18a5e3f444bf859983efeb9d1a195
Justin Lambert <jlambert@eml.cc>
2013-10-02 08:57:13 -0600

    default to validate certs, add rpsec tests

--- manifests/mod/authnz_ldap.pp
+++ manifests/mod/authnz_ldap.pp
@@ -1,13 +1,22 @@
 class apache::mod::authnz_ldap(
-  $ldap_conf = 'apache/mod/authnz_ldap.conf.erb'
+  $verifyServerCert = true,
 ) {
   include apache::mod::ldap
   apache::mod { 'authnz_ldap': }
-  file { 'authnz_ldap.conf':
-    ensure  => file,
-    path    => "${apache::mod_dir}/authnz_ldap.conf",
-    content => template($ldap_conf),
-    before  => File[$apache::mod_dir],
-    notify  => Service['httpd'],
+
+  if $verifyServerCert == true {
+    file { 'authnz_ldap.conf':
+      ensure  => absent,
+      path    => "${apache::mod_dir}/authnz_ldap.conf",
+      notify  => Service['httpd'],
+    }
+  } else {
+    file { 'authnz_ldap.conf':
+      ensure  => file,
+      path    => "${apache::mod_dir}/authnz_ldap.conf",
+      content => 'LDAPVerifyServerCert off',
+      before  => File[$apache::mod_dir],
+      notify  => Service['httpd'],
+    }
   }
 }
