fca1cc7551ef7ef84b1515b6977332fb94caf8fe
Pablo Fredrikson <pfredrikson@brivincorp.com>
2013-10-23 16:00:20 -0300

    added some tests

--- spec/defines/vhost_spec.rb
+++ spec/defines/vhost_spec.rb
@@ -702,6 +702,19 @@ describe 'apache::vhost', :type => :define do
           it { should contain_file("25-#{title}.conf").with_content %r{  Redirect permanent /logout http://10\.0\.0\.10/test} }
         end
 
+       describe 'redirect match rules' do
+        let :params do
+         default_params.merge({
+           :redirectmatch_status => [
+             '404',
+           ],
+           :redirectmatch_regexp   => [
+             '/\\.git(/.*|$)',
+           ],
+         })
+        it { should contain_file("25-#{title}.conf").with_content %r{  RedirectMatch 404 /\\\.git(/\.*|$)} }
+       end
+
         describe 'with a directoryindex specified' do
           let :params do
             default_params.merge({
