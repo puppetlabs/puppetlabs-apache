739cd5f76f2fcb04bc8a8d16d50c1a01e838c382
Kyle Sexton <ks@mocker.org>
2013-11-13 08:27:35 -0600

    Add WSGIPythonPath as an optional parameter to mod_wsgi.  This parameter is used to specify additional directories to search for Python modules as described here: https://code.google.com/p/modwsgi/wiki/ConfigurationDirectives#WSGIPythonPath

--- manifests/mod/wsgi.pp
+++ manifests/mod/wsgi.pp
@@ -1,11 +1,13 @@
 class apache::mod::wsgi (
   $wsgi_socket_prefix = undef,
+  $wsgi_python_path   = undef,
   $wsgi_python_home   = undef,
 ){
   apache::mod { 'wsgi': }
 
   # Template uses:
   # - $wsgi_socket_prefix
+  # - $wsgi_python_path
   # - $wsgi_python_home
   file {'wsgi.conf':
     ensure  => file,
