f749375184e0bb7bbc3f5a63a7e6b4d8183a1f50
Igor Galić <i.galic@brainsware.org>
2013-11-21 19:11:17 +0100

    Set SSLOptions StdEnvVars in server context
    We set `SSLOptions` now in server context and we set it to `StdEnvVars`.
    Thus, we no longer need the <FilesMatch > in server context to set it
    for php, CGI and SSI applications.
    We allow it to be overridden by the `apache::mod::ssl` class parameter
    `ssl_options`. Further, we allow it to be overridden in directory
    context.
    Documentation for all of these has been extended and tests have been
    adapted to make sense in this new context. This commit fixes #487

--- manifests/mod/ssl.pp
+++ manifests/mod/ssl.pp
@@ -1,5 +1,6 @@
 class apache::mod::ssl (
   $ssl_compression = false,
+  $ssl_options     = [ 'StdEnvVars' ],
 ) {
   $session_cache = $::osfamily ? {
     'debian'  => '${APACHE_RUN_DIR}/ssl_scache(512000)',
@@ -13,7 +14,7 @@ class apache::mod::ssl (
   }
   apache::mod { 'ssl': }
 
-  # Template uses $ssl_compression, $session_cache, $ssl_mutex
+  # Template uses $ssl_compression, $ssl_options, $session_cache, $ssl_mutex
   file { 'ssl.conf':
     ensure  => file,
     path    => "${apache::mod_dir}/ssl.conf",
