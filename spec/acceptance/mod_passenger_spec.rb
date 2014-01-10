1a7936a89d36acb1d24a737ed5d3c61fe0bdf539
jonoterc <jon_obuchowski@terc.edu>
2014-01-09 19:35:51 -0500

    add spec/acceptance coverage for passenger module
    add basic spec/acceptance tests for passenger module; these verify a
    default installation as well as passing in module loading parameters.
    new tests revealed undocumented Redhat dependencies on passenger-
    specific repository as well as the EPEL repository; as a first step
    these dependencies are being manually resolved during setup for the
    acceptance testing suite.
    
    also, documented new apache::mod::passenger parameters (originatinig
    with apache::mod) in README.passenger.md

---
+++ spec/acceptance/mod_passenger_spec.rb
@@ -0,0 +1,115 @@
+require 'spec_helper_acceptance'
+
+describe 'apache::mod::passenger class' do
+  case fact('osfamily')
+  when 'Debian'
+    mod_dir      = '/etc/apache2/mods-available'
+    service_name = 'apache2'
+  when 'RedHat'
+    mod_dir      = '/etc/httpd/conf.d'
+    service_name = 'httpd'
+  when 'FreeBSD'
+    mod_dir      = '/usr/local/etc/apache22/Modules'
+    service_name = 'apache22'
+  end
+
+  context "default passenger config" do
+    it 'succeeds in puppeting passenger' do
+      pp= <<-EOS
+        class { 'apache': }
+        class { 'apache::mod::passenger': }
+        apache::vhost { 'passenger.example.com':
+          port    => '80',
+          docroot => '/var/www/passenger/public',
+          custom_fragment => "PassengerRuby  /usr/bin/ruby\\nRailsEnv  development" ,
+        }
+        host { 'passenger.example.com': ip => '127.0.0.1', }
+        file { '/var/www/passenger':
+          ensure  => directory,
+        }
+        file { '/var/www/passenger/tmp':
+          ensure  => directory,
+        }
+        file { '/var/www/passenger/public':
+          ensure  => directory,
+        }
+        file { '/var/www/passenger/config.ru':
+          ensure  => file,
+          content => "app = proc { |env| [200, { \\"Content-Type\\" => \\"text/html\\" }, [\\"hello <b>world</b>\\"]]
+}\\nrun app",
+        }
+      EOS
+      apply_manifest(pp, :catch_failures => true)
+    end
+
+    describe service(service_name) do
+      it { should be_enabled }
+      it { should be_running }
+    end
+
+    describe file("#{mod_dir}/passenger.conf") do
+      it { should contain /^<IfModule mod_passenger\.c>$/ }
+      it { should contain /^  PassengerRuby \/usr\/bin\/ruby$/ }
+    end
+
+    it 'should answer to passenger.example.com' do
+      shell("/usr/bin/curl passenger.example.com:80") do |r|
+        r.stdout.should =~ /hello <b>world<\/b>/
+        r.exit_code.should == 0
+      end
+    end
+  end
+
+  context "customized passenger config" do
+    it 'succeeds in puppeting passenger' do
+      pp= <<-EOS
+        class { 'apache': }
+        class { 'apache::mod::passenger':
+          mod_package_ensure => 'present',
+          mod_id             => 'passenger_module',
+          mod_lib            => 'mod_passenger.so',
+          mod_lib_path       => $apache::params::lib_path,
+          mod_path           => "${apache::params::lib_path}/mod_passenger.so",
+        }
+        apache::vhost { 'passenger.example.com':
+          port    => '80',
+          docroot => '/var/www/passenger/public',
+          custom_fragment => "PassengerRuby  /usr/bin/ruby\\nRailsEnv  development" ,
+        }
+        host { 'passenger.example.com': ip => '127.0.0.1', }
+        file { '/var/www/passenger':
+          ensure  => directory,
+        }
+        file { '/var/www/passenger/tmp':
+          ensure  => directory,
+        }
+        file { '/var/www/passenger/public':
+          ensure  => directory,
+        }
+        file { '/var/www/passenger/config.ru':
+          ensure  => file,
+          content => "app = proc { |env| [200, { \\"Content-Type\\" => \\"text/html\\" }, [\\"hello <b>world</b>\\"]]
+}\\nrun app",
+        }
+      EOS
+      apply_manifest(pp, :catch_failures => true)
+    end
+
+    describe service(service_name) do
+      it { should be_enabled }
+      it { should be_running }
+    end
+
+    describe file("#{mod_dir}/passenger.conf") do
+      it { should contain /^<IfModule mod_passenger\.c>$/ }
+      it { should contain /^  PassengerRuby \/usr\/bin\/ruby$/ }
+    end
+
+    it 'should answer to passenger.example.com' do
+      shell("/usr/bin/curl passenger.example.com:80") do |r|
+        r.stdout.should =~ /hello <b>world<\/b>/
+        r.exit_code.should == 0
+      end
+    end
+  end
+end
--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -13,6 +13,29 @@ hosts.each do |host|
     on host, 'gem install puppet --no-ri --no-rdoc'
     on host, "mkdir -p #{host['distmoduledir']}"
   end
+
+  # host-specific repositories
+  repos = {
+    'centos-64-x64' => [
+        [
+          'http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm' ,
+          nil
+        ],
+        [
+          'http://passenger.stealthymonkeys.com/rhel/6/passenger-release.noarch.rpm',
+          'http://passenger.stealthymonkeys.com/RPM-GPG-KEY-stealthymonkeys.asc'
+        ]
+      ]
+  }
+
+  host_repos = repos[host.to_s] || []
+  host_repos.each do |repo_url,gpg_key|
+    puts "configuring #{host}-specific repo #{repo_url.inspect}"
+    unless gpg_key.nil?
+      shell("rpm --import #{gpg_key}")
+    end
+    shell("rpm -Uvh #{repo_url}")
+  end
 end
 
 RSpec.configure do |c|
