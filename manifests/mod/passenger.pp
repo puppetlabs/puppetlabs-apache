30e5cc8149b0e79477b6274a95f0bee294f7c7df
jonoterc <jon_obuchowski@terc.edu>
2014-02-04 00:24:10 -0500

    enable overriding mod-level parameters for apache::mod::passenger
    apache::mod::passenger didn't accept/pass (optional) mod parameters
    through to apache::mod, so only auto-generated module parameters were
    used.  Without this option apache::mod::passenger could not be used in
    conjunction with passenger modules beyond those generated by OS package
    system (in particular, modules compiled via
    passenger-install-apache2-module, which use differing paths).  This
    commit enables passing in all parameters used with "apache::mod",
    documented in the passenger README
    
    includes basic spec/acceptance tests; verifying a default installation
    as well as passing in module loading parameters.
    
    new tests revealed undocumented Redhat dependencies on a passenger-
    specific repository as well as the EPEL repository; as a first step
    these dependencies are being manually resolved during setup for the
    acceptance testing suite.

--- manifests/mod/passenger.pp
+++ manifests/mod/passenger.pp
@@ -11,20 +11,42 @@ class apache::mod::passenger (
   $passenger_ruby                 = $::apache::params::passenger_ruby,
   $passenger_max_pool_size        = undef,
   $passenger_use_global_queue     = undef,
+  $mod_package                    = undef,
+  $mod_package_ensure             = undef,
+  $mod_lib                        = undef,
+  $mod_lib_path                   = undef,
+  $mod_id                         = undef,
+  $mod_path                       = undef,
 ) {
-  if $::osfamily == 'FreeBSD' {
-    ::apache::mod { 'passenger':
-      lib_path => "${passenger_root}/buildout/apache2"
-    }
-  } else {
-    ::apache::mod { 'passenger': }
-  }
-
   # Managed by the package, but declare it to avoid purging
   if $passenger_conf_package_file {
     file { 'passenger_package.conf':
       path => "${::apache::mod_dir}/${passenger_conf_package_file}",
     }
+  }
+
+  $_package = $mod_package
+  $_package_ensure = $mod_package_ensure
+  $_lib = $mod_lib
+  if $::osfamily == 'FreeBSD' {
+    if $mod_lib_path {
+      $_lib_path = $mod_lib_path
+    } else {
+      $_lib_path = "${passenger_root}/buildout/apache2"
+    }
+  } else {
+    $_lib_path = $mod_lib_path
+  }
+
+  $_id = $mod_id
+  $_path = $mod_path
+  ::apache::mod { 'passenger':
+    package        => $_package,
+    package_ensure => $_package_ensure,
+    lib            => $_lib,
+    lib_path       => $_lib_path,
+    id             => $_id,
+    path           => $_path,
   }
 
   # Template uses:
