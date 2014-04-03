198aa99e8e5925cd1bdf9bfd832f0e923e4e8a2b
Angel L. Mateo <amateo@um.es>
2014-04-03 13:09:37 +0200

    Add parameters to configure negotiation module

--- manifests/mod/negotiation.pp
+++ manifests/mod/negotiation.pp
@@ -1,4 +1,13 @@
-class apache::mod::negotiation {
+class apache::mod::negotiation (
+  $force_language_priority = 'Prefer Fallback',
+  $language_priority = [ 'en', 'ca', 'cs', 'da', 'de', 'el', 'eo', 'es', 'et',
+                        'fr', 'he', 'hr', 'it', 'ja', 'ko', 'ltz', 'nl', 'nn',
+                        'no', 'pl', 'pt', 'pt-BR', 'ru', 'sv', 'zh-CN',
+                        'zh-TW' ],
+) {
+  validate_string($force_language_priority)
+  validate_array($language_priority)
+
   ::apache::mod { 'negotiation': }
   # Template uses no variables
   file { 'negotiation.conf':
