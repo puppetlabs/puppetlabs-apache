e8d9ea1ecce54307a94a113a43a0e1a6662f1aed
Angel L. Mateo <amateo@um.es>
2014-04-03 13:09:37 +0200

    Add content, template and source parameters in order to be able to configure this module

--- manifests/mod/negotiation.pp
+++ manifests/mod/negotiation.pp
@@ -1,10 +1,27 @@
-class apache::mod::negotiation {
+class apache::mod::negotiation (
+  $content  = '',
+  $template = 'apache/mod/negotiation.conf.erb',
+  $source   = '',
+) {
+  $manage_content = $content ? {
+    ''      => $template ? {
+      ''      => undef,
+      default => template($template),
+    },
+    default => $content,
+  }
+  $manage_source = $source ? {
+    ''      => undef,
+    default => $source,
+  }
+
   ::apache::mod { 'negotiation': }
   # Template uses no variables
   file { 'negotiation.conf':
     ensure  => file,
     path    => "${::apache::mod_dir}/negotiation.conf",
-    content => template('apache/mod/negotiation.conf.erb'),
+    content => $manage_content,
+    source  => $manage_source,
     require => Exec["mkdir ${::apache::mod_dir}"],
     before  => File[$::apache::mod_dir],
     notify  => Service['httpd'],
