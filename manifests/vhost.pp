6da2391915c3a0c4bf2b0dc99986dcdb752a9da9
Baird, Josh <jbaird@follett.com>
2014-05-01 13:32:09 -0500

    fix resource title

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -249,16 +249,18 @@ define apache::vhost(
   # This ensures that the docroot exists
   # But enables it to be specified across multiple vhost resources
   if ! defined(File[$docroot]) {
-    exec { "mkdocroot":
-      command => 'mkdir -p $docroot',
+    exec { $docroot:
+      command => "mkdir -p $docroot",
       creates => $docroot,
+      path    => ["/bin", "/usr/bin"],
       require => Package['httpd'],
     }
     file { $docroot:
       ensure  => directory,
       owner   => $docroot_owner,
       group   => $docroot_group,
-      require => Exec['mkdocroot'],
+      mode    => $docroot_mode,
+      require => Exec[$docroot],
     }
   }
 
