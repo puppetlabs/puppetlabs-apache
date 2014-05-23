6eb81e790457c61a64e8c072d77899b709c744cd
Angel L. Mateo <amateo@um.es>
2014-05-23 13:09:15 +0200

    Add parameters to provide content, template or source to the php module

--- manifests/mod/php.pp
+++ manifests/mod/php.pp
@@ -2,10 +2,26 @@ class apache::mod::php (
   $package_name   = undef,
   $package_ensure = 'present',
   $path           = undef,
+  $content        = '',
+  $template       = 'apache/mod/php5.conf.erb',
+  $source         = '',
 ) {
   if ! defined(Class['apache::mod::prefork']) {
     fail('apache::mod::php requires apache::mod::prefork; please enable mpm_module => \'prefork\' on Class[\'apache\']')
   }
+  $manage_content = $source ? {
+    ''      => $content ? {
+      ''      => template($template),
+      default => $content,
+    },
+    default => undef,
+  }
+
+  $manage_source = $source ? {
+    ''      => undef,
+    default => $source,
+  }
+
   ::apache::mod { 'php5':
     package        => $package_name,
     package_ensure => $package_ensure,
@@ -19,7 +35,8 @@ class apache::mod::php (
   file { 'php5.conf':
     ensure  => file,
     path    => "${::apache::mod_dir}/php5.conf",
-    content => template('apache/mod/php5.conf.erb'),
+    content => $manage_content,
+    source  => $manage_source,
     require => [
       Class['::apache::mod::prefork'],
       Exec["mkdir ${::apache::mod_dir}"],
