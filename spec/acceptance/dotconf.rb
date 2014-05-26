bce9951f34c04c8dc0e63ae59dc6922ec70866c2
Angel L. Mateo <amateo@um.es>
2014-05-26 14:47:14 +0200

    A more complete acceptance test.

--- spec/acceptance/dotconf.rb
+++ spec/acceptance/dotconf.rb
@@ -17,4 +17,27 @@ describe 'apache::dotconf define', :unless => UNSUPPORTED_PLATFORMS.include?(fac
       it { should_not be_file }
     end
   end
+
+  context 'check parameters' do
+    it 'test with parameters' do
+      pp = <<-EOS
+        class { 'apache': }
+        apache::dotconf {'test':
+          ensure  => 'present',
+          content => 'somecontent',
+          owner   => 'root',
+          group   => 'root',
+          mode    => '0640',
+          path    => '/etc/apache/conf.d/',
+        }
+      EOS
+
+      apply_manifest(pp, :catch_failures => true)
+    end
+
+    describe file('/etc/apache/conf.d/test.conf') do
+      it { should be_file }
+      it { should containt 'somecontent' }
+    end
+  end
 end
