460775670a3297c0850fda53c82af2d3272ce827
Phil Fenstermacher <pcfens@wm.edu>
2014-06-02 13:27:02 -0400

    Add better native support for Apache 2.4 when mod_access_compat isn't installed/enabled

--- manifests/mod/userdir.pp
+++ manifests/mod/userdir.pp
@@ -2,10 +2,11 @@ class apache::mod::userdir (
   $home = '/home',
   $dir = 'public_html',
   $disable_root = true,
+  $apache_version = $::apache::apache_version,
 ) {
   ::apache::mod { 'userdir': }
 
-  # Template uses $home, $dir, $disable_root
+  # Template uses $home, $dir, $disable_root, $apache_version
   file { 'userdir.conf':
     ensure  => file,
     path    => "${::apache::mod_dir}/userdir.conf",
