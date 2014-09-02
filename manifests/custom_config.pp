f4ecfa2b9ab94f37dfd663f10e4bc02a242c1d3f
Morgan Haskel <morgan@puppetlabs.com>
2014-09-02 16:45:37 -0400

    Add defined type for handling custom configs
    Initially I was looking to handle custom configs by creating a temporary
    file, validating that file, and creating a file under apache's
    `$confd_dir` only if that validation passed. Ubuntu caused issues for me
    though, and apachectl was unable to validate config files under
    non-default paths. Thus, I ended up with a file resource and two execs,
    to remove the config if invalid, and notify the httpd service if valid.
    
    There were some bugs with existing acceptance tests, so fixed those too.

---
+++ manifests/custom_config.pp
@@ -0,0 +1,60 @@
+# See README.md for usage information
+define apache::custom_config (
+  $ensure         = 'present',
+  $confdir        = $::apache::confd_dir,
+  $content        = undef,
+  $priority       = '25',
+  $source         = undef,
+  $verify_command = '/usr/sbin/apachectl -t',
+  $verify_config  = true,
+) {
+
+  if $content and $source {
+    fail('Only one of $content and $source can be specified.')
+  }
+
+  if $ensure == 'present' and ! $content and ! $source {
+    fail('One of $content and $source must be specified.')
+  }
+
+  validate_re($ensure, '^(present|absent)$',
+  "${ensure} is not supported for ensure.
+  Allowed values are 'present' and 'absent'.")
+
+  validate_bool($verify_config)
+
+  ## Apache include does not always work with spaces in the filename
+  $filename  = regsubst($name, ' ', '_', 'G')
+
+  if ! $verify_config or $ensure == 'absent' {
+    $notifies = Service['httpd']
+  } else {
+    $notifies = undef
+  }
+
+  file { "apache_${name}":
+    ensure  => $ensure,
+    path    => "${confdir}/${priority}-${filename}.conf",
+    content => $content,
+    source  => $source,
+    require => Package['httpd'],
+    notify  => $notifies,
+  }
+
+  if $ensure == 'present' and $verify_config {
+    exec { "service notify for ${name}":
+      command     => $verify_command,
+      subscribe   => File["apache_${name}"],
+      refreshonly => true,
+      notify      => Service['httpd'],
+      before      => Exec["remove ${name} if invalid"],
+    }
+
+    exec { "remove ${name} if invalid":
+      command     => "/bin/rm ${confdir}/${priority}-${filename}.conf",
+      unless      => $verify_command,
+      subscribe   => File["apache_${name}"],
+      refreshonly => true,
+    }
+  }
+}
