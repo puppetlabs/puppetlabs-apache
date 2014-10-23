b1b7689a7f0d15ab490873b4b68b754ae4f6e7c6
Tim Hartmann <tfhartmann@gmail.com>
2014-10-23 15:50:15 -0400

    Linting and adding some tests
    Adding tests

--- tests/vhost_proxypass.pp
+++ tests/vhost_proxypass.pp
@@ -9,37 +9,40 @@ class { 'apache': }
 
 # Most basic vhost with proxy_pass
 apache::vhost { 'first.example.com':
-  port        => 80,
-  docroot     => '/var/www/first',
-  proxy_pass  => [
+  port       => 80,
+  docroot    => '/var/www/first',
+  proxy_pass => [
     {
-      'path'   => '/first',
-      'url'    => "http://localhost:8080/first"
+      'path' => '/first',
+      'url'  => 'http://localhost:8080/first'
     },
   ],
 }
 
 # vhost with proxy_pass and parameters
 apache::vhost { 'second.example.com':
-  port        => 80,
-  docroot     => '/var/www/second',
-  proxy_pass  => [
+  port       => 80,
+  docroot    => '/var/www/second',
+  proxy_pass => [
     {
       'path'   => '/second',
-      'url'    => "http://localhost:8080/second",
-      'params' => {'retry' => '0', 'timeout' => '5'}
+      'url'    => 'http://localhost:8080/second',
+      'params' => {
+        'retry'   => '0',
+        'timeout' => '5'
+        }
     },
   ],
 }
 
 # vhost with proxy_pass and keywords
 apache::vhost { 'third.example.com':
-  port        => 80,
-  docroot     => '/var/www/third',
-  proxy_pass  => [
+  port       => 80,
+  docroot    => '/var/www/third',
+  proxy_pass => [
     {
-      'path'   => '/third',
-      'url'    => "http://localhost:8080/third",
+      'path'     => '/third',
+      'url'      => 'http://localhost:8080/third',
       'keywords' => ['noquery', 'interpolate']
     },
   ],
@@ -47,13 +50,16 @@ apache::vhost { 'third.example.com':
 
 # vhost with proxy_pass, parameters and keywords
 apache::vhost { 'fourth.example.com':
-  port        => 80,
-  docroot     => '/var/www/fourth',
-  proxy_pass  => [
+  port       => 80,
+  docroot    => '/var/www/fourth',
+  proxy_pass => [
     {
-      'path'   => '/fourth',
-      'url'    => "http://localhost:8080/fourth",
-      'params' => {'retry' => '0', 'timeout' => '5'},
+      'path'     => '/fourth',
+      'url'      => 'http://localhost:8080/fourth',
+      'params'   => {
+        'retry'   => '0',
+        'timeout' => '5'
+        },
       'keywords' => ['noquery', 'interpolate']
     },
   ],
