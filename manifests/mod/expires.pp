772e838e8c6e85ffc0b0092b2bd49daab73d51b2
Brent Clark <brent.clark@hetzner.co.za>
2014-12-01 15:29:39 +0200

    Manage Apaches mod_expires types.

--- manifests/mod/expires.pp
+++ manifests/mod/expires.pp
@@ -1,3 +1,24 @@
-class apache::mod::expires {
+class apache::mod::expires(
+  $expiresdefault = undef,
+  $expiresactive  = 'On',
+  $types          = {
+    'image/gif'    => 'A2592000',
+    'image/jpeg'   => 'A2592000',
+    'image/jpg'    => 'A2592000',
+    'image/png'    => 'A2592000',
+    'image/x-icon' => 'A2592000',
+    'image/ico'    => 'A2592000',
+    'image/bmp'    => 'A2592000',
+  },
+){
   ::apache::mod { 'expires': }
+
+  file { 'expires.conf':
+    ensure  => file,
+    path    => "${::apache::mod_dir}/expires.conf",
+    content => template('apache/mod/expires.conf.erb'),
+    require => Exec["mkdir ${::apache::mod_dir}"],
+    before  => File[$::apache::mod_dir],
+    notify  => Service['httpd'],
+  }
 }
