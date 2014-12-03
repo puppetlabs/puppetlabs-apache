54bf1a847289e2e99ebe1f0fc8e6f8fa7d78dc1d
Justin Stoller <justin.stoller@gmail.com>
2014-12-02 16:43:07 -0800

    Fix uninitialized variable lint

--- manifests/mod/version.pp
+++ manifests/mod/version.pp
@@ -1,4 +1,6 @@
-class apache::mod::version {
+class apache::mod::version(
+  $apache_version = $::apache::apache_version
+) {
 
   if ($::osfamily == 'debian' and versioncmp($apache_version, '2.4') >= 0) {
     warning("${module_name}: module version_module is built-in and can't be loaded")
