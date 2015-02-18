3a702ecf4e3ae3c474743feba0ecbbde98cebe29
Travis Fields <travis@puppetlabs.com>
2015-02-18 10:34:51 -0800

    SuPHP acceptance fixes?, chasing the test bug that is timing

--- spec/acceptance/mod_suphp_spec.rb
+++ spec/acceptance/mod_suphp_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper_acceptance'
 
 describe 'apache::mod::suphp class', :unless => UNSUPPORTED_PLATFORMS.include?(fact('osfamily')) do
   case fact('osfamily')
-  when 'Debian'
-    context "default suphp config" do
-      it 'succeeds in puppeting suphp' do
-        pp = <<-EOS
+    when 'Debian'
+      context "default suphp config" do
+        it 'succeeds in puppeting suphp' do
+          pp = <<-EOS
           class { 'apache':
             mpm_module => 'prefork',
           }
@@ -22,24 +22,28 @@ describe 'apache::mod::suphp class', :unless => UNSUPPORTED_PLATFORMS.include?(f
             group   => 'daemon',
             content => "<?php echo get_current_user(); ?>\\n",
           }
-        EOS
-        apply_manifest(pp, :catch_failures => true)
-      end
+          EOS
+          apply_manifest(pp, :catch_failures => true)
+        end
 
-      describe service('apache2') do
-        it { is_expected.to be_enabled }
-        it { is_expected.to be_running }
-      end
+        describe service('apache2') do
+          it { is_expected.to be_enabled }
+          it { is_expected.to be_running }
+        end
 
-      it 'should answer to suphp.example.com' do
-        shell("/bin/sleep 10")
-        shell("/usr/bin/curl suphp.example.com:80") do |r|
-          expect(r.stdout).to match(/^daemon$/)
-          expect(r.exit_code).to eq(0)
+        it 'should answer to suphp.example.com' do
+          timeout = 0
+          loop do
+            r = shell('curl suphp.example.com:80')
+            timeout += 1
+            break if timeout > 20 || r.stdout =~ /^daemon$/
+            sleep(1)
+          end
+          shell("/usr/bin/curl suphp.example.com:80") do |r|
+            expect(r.stdout).to match(/^daemon$/)
+            expect(r.exit_code).to eq(0)
+          end
         end
       end
-    end
-  when 'RedHat'
-    # Not implemented yet
   end
 end
