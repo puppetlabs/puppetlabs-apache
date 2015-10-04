4c87d22f47b0d89dc69496947c8deab841ae4d25
Matthias Pigulla <mp@webfactory.de>
2015-10-04 12:39:58 +0200

    Add option to configure the include pattern for the vhost_enable dir

--- manifests/params.pp
+++ manifests/params.pp
@@ -32,6 +32,8 @@ class apache::params inherits ::apache::version {
   # should we use systemd module?
   $use_systemd = true
 
+  $vhost_include_pattern = '*'
+
   if $::operatingsystem == 'Ubuntu' and $::lsbdistrelease == '10.04' {
     $verify_command = '/usr/sbin/apache2ctl -t'
   } else {
