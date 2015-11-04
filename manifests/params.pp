1dfc02d9036e501570aebabc8d02945073e33a6d
wolfgang hotwagner <wolfgang.hotwagner@toscom.at>
2015-11-04 23:12:42 +0100

    Added support for modsecurity parameter SecPcreMatchLimit and SecPcreMatchLimitRecursion
    fixed default variables for secpcrematchlimit(recursion) in params.pp
    
    added documentation for SecPcreMatchLimit and SecPcreMatchLimitRecursion. also changed the default value to 1500(its modsecurity's default value)
    
    removed modsec_-prefix from variable names

--- manifests/params.pp
+++ manifests/params.pp
@@ -153,6 +153,8 @@ class apache::params inherits ::apache::version {
     $modsec_crs_package   = 'mod_security_crs'
     $modsec_crs_path      = '/usr/lib/modsecurity.d'
     $modsec_dir           = '/etc/httpd/modsecurity.d'
+    $secpcrematchlimit = 1500
+    $secpcrematchlimitrecursion = 1500
     $modsec_secruleengine = 'On'
     $modsec_default_rules = [
       'base_rules/modsecurity_35_bad_robots.data',
@@ -248,6 +250,8 @@ class apache::params inherits ::apache::version {
     $modsec_crs_package   = 'modsecurity-crs'
     $modsec_crs_path      = '/usr/share/modsecurity-crs'
     $modsec_dir           = '/etc/modsecurity'
+    $secpcrematchlimit = 1500
+    $secpcrematchlimitrecursion = 1500
     $modsec_secruleengine = 'On'
     $modsec_default_rules = [
       'base_rules/modsecurity_35_bad_robots.data',
