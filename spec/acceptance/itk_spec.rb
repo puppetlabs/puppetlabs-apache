893e02dbb1a41e038a3fae6116aae475527b5b69
David Schmitt <david.schmitt@puppetlabs.com>
2015-12-09 14:16:48 +0000

    (MAINT) fixup ITK tests for older debian-based releases

--- spec/acceptance/itk_spec.rb
+++ spec/acceptance/itk_spec.rb
@@ -3,20 +3,37 @@ require 'spec_helper_acceptance'
 case fact('osfamily')
 when 'Debian'
   service_name = 'apache2'
+  majrelease = fact('operatingsystemmajrelease')
+  if [ '6', '7', '10.04', '12.04'].contains(majrelease)
+    variant = :itk_only
+  else
+    variant = :prefork
+  end
 when 'FreeBSD'
   service_name = 'apache24'
+  majrelease = fact('operatingsystemmajrelease')
+  variant = :prefork
 end
 
 describe 'apache::mod::itk class', :if => service_name do
   describe 'running puppet code' do
     # Using puppet_apply as a helper
     it 'should work with no errors' do
-      pp = <<-EOS
-        class { 'apache':
-          mpm_module => 'prefork',
-        }
-        class { 'apache::mod::itk': }
-      EOS
+      pp = case variant
+           when :prefork
+             <<-EOS
+               class { 'apache':
+                 mpm_module => 'prefork',
+               }
+               class { 'apache::mod::itk': }
+             EOS
+           when :itk_only
+             <<-EOS
+               class { 'apache':
+                 mpm_module => 'itk',
+               }
+             EOS
+           end
 
       # Run it twice and test for idempotency
       apply_manifest(pp, :catch_failures => true)
