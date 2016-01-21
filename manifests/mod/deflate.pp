0249e8b0fb3c4e892463a85cbd6dad46a2bf7223
pfgdrupalas <pfgdrupalas@users.noreply.github.com>
2016-01-21 05:50:03 -0600

    Update deflate.pp
    added '-p' to apache::mod_dir

--- manifests/mod/deflate.pp
+++ manifests/mod/deflate.pp
@@ -18,7 +18,7 @@ class apache::mod::deflate (
     ensure  => file,
     path    => "${::apache::mod_dir}/deflate.conf",
     content => template('apache/mod/deflate.conf.erb'),
-    require => Exec["mkdir ${::apache::mod_dir}"],
+    require => Exec["mkdir -p ${::apache::mod_dir}"],
     before  => File[$::apache::mod_dir],
     notify  => Class['apache::service'],
   }
