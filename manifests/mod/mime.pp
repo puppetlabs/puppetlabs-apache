02ef13634b259ce99868ba197650049f416c5671
pfgdrupalas <pfgdrupalas@users.noreply.github.com>
2016-01-21 06:30:50 -0600

    Update mime.pp
    added '-p' to apache::mod_dir

--- manifests/mod/mime.pp
+++ manifests/mod/mime.pp
@@ -9,7 +9,7 @@ class apache::mod::mime (
     ensure  => file,
     path    => "${::apache::mod_dir}/mime.conf",
     content => template('apache/mod/mime.conf.erb'),
-    require => Exec["mkdir ${::apache::mod_dir}"],
+    require => Exec["mkdir -p ${::apache::mod_dir}"],
     before  => File[$::apache::mod_dir],
     notify  => Class['apache::service'],
   }
