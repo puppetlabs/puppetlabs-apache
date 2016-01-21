fa8338b6602192349fd24a18129e7c46926f20a1
pfgdrupalas <pfgdrupalas@users.noreply.github.com>
2016-01-21 07:14:01 -0600

    Update suphp.pp
    added '-p' to apache::mod_dir

--- manifests/mod/suphp.pp
+++ manifests/mod/suphp.pp
@@ -6,7 +6,7 @@ class apache::mod::suphp (
     ensure  => file,
     path    => "${::apache::mod_dir}/suphp.conf",
     content => template('apache/mod/suphp.conf.erb'),
-    require => Exec["mkdir ${::apache::mod_dir}"],
+    require => Exec["mkdir -p ${::apache::mod_dir}"],
     before  => File[$::apache::mod_dir],
     notify  => Class['apache::service'],
   }
