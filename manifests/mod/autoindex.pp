2cd29367be868684e1c2d7a73f4b7b7abce317ea
pfgdrupalas <pfgdrupalas@users.noreply.github.com>
2016-01-21 05:48:20 -0600

    Update autoindex.pp
    added '-p' to apache::mod_dir

--- manifests/mod/autoindex.pp
+++ manifests/mod/autoindex.pp
@@ -5,7 +5,7 @@ class apache::mod::autoindex {
     ensure  => file,
     path    => "${::apache::mod_dir}/autoindex.conf",
     content => template('apache/mod/autoindex.conf.erb'),
-    require => Exec["mkdir ${::apache::mod_dir}"],
+    require => Exec["mkdir -p ${::apache::mod_dir}"],
     before  => File[$::apache::mod_dir],
     notify  => Class['apache::service'],
   }
