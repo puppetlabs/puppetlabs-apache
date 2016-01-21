19084d8345fddef62be758a486415cfc2e35efb3
pfgdrupalas <pfgdrupalas@users.noreply.github.com>
2016-01-21 05:51:14 -0600

    Update disk_cache.pp
    added '-p' to apache::mod_dir

--- manifests/mod/disk_cache.pp
+++ manifests/mod/disk_cache.pp
@@ -33,7 +33,7 @@ class apache::mod::disk_cache (
     ensure  => file,
     path    => "${::apache::mod_dir}/disk_cache.conf",
     content => template('apache/mod/disk_cache.conf.erb'),
-    require => Exec["mkdir ${::apache::mod_dir}"],
+    require => Exec["mkdir -p ${::apache::mod_dir}"],
     before  => File[$::apache::mod_dir],
     notify  => Class['apache::service'],
   }
