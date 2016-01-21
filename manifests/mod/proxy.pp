1d7b3074aea33aa2de9d8659591c30b46391b8ff
pfgdrupalas <pfgdrupalas@users.noreply.github.com>
2016-01-21 06:39:27 -0600

    Update proxy.pp
    added '-p' to apache::mod_dir

--- manifests/mod/proxy.pp
+++ manifests/mod/proxy.pp
@@ -9,7 +9,7 @@ class apache::mod::proxy (
     ensure  => file,
     path    => "${::apache::mod_dir}/proxy.conf",
     content => template('apache/mod/proxy.conf.erb'),
-    require => Exec["mkdir ${::apache::mod_dir}"],
+    require => Exec["mkdir -p ${::apache::mod_dir}"],
     before  => File[$::apache::mod_dir],
     notify  => Class['apache::service'],
   }
