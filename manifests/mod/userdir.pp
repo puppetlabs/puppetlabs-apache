91fb605219ce13d3c567e3712e26b2229248a0ad
pfgdrupalas <pfgdrupalas@users.noreply.github.com>
2016-01-21 07:15:22 -0600

    Update userdir.pp
    added '-p' to apache::mod_dir

--- manifests/mod/userdir.pp
+++ manifests/mod/userdir.pp
@@ -12,7 +12,7 @@ class apache::mod::userdir (
     ensure  => file,
     path    => "${::apache::mod_dir}/userdir.conf",
     content => template('apache/mod/userdir.conf.erb'),
-    require => Exec["mkdir ${::apache::mod_dir}"],
+    require => Exec["mkdir -p ${::apache::mod_dir}"],
     before  => File[$::apache::mod_dir],
     notify  => Class['apache::service'],
   }
