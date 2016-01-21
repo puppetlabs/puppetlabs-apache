3fc47ed622c7c05f8a53104522d641b8a0fc45fe
pfgdrupalas <pfgdrupalas@users.noreply.github.com>
2016-01-21 06:28:58 -0600

    Update geoip.pp
    added '-p' to apache::mod_dir

--- manifests/mod/geoip.pp
+++ manifests/mod/geoip.pp
@@ -23,7 +23,7 @@ class apache::mod::geoip (
     ensure  => file,
     path    => "${::apache::mod_dir}/geoip.conf",
     content => template('apache/mod/geoip.conf.erb'),
-    require => Exec["mkdir ${::apache::mod_dir}"],
+    require => Exec["mkdir -p ${::apache::mod_dir}"],
     before  => File[$::apache::mod_dir],
     notify  => Class['apache::service'],
   }
