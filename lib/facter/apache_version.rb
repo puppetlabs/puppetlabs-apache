5e431279017e900966fc843d75a97c943448d016
Joseph Yaworski <joseph.yaworski@fireeye.com>
2016-01-29 14:51:43 -0500

    Add apache_version fact

---
+++ lib/facter/apache_version.rb
@@ -0,0 +1,8 @@
+Facter.add(:apache_version) do
+  setcode do
+    if Facter::Util::Resolution.which('apachectl')
+      apache_version = Facter::Util::Resolution.exec('apachectl -v 2>&1')
+      %r{^Server version: Apache\/([\w\.]+) \(([\w]+)\)}.match(apache_version)[1]
+    end
+  end
+end
