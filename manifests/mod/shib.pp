5047e6a804347be6dbf848e0fd236c479bb029be
gvdb1967 <gvdb1967@gmail.com>
2016-02-06 00:32:17 +0100

    added extra var.:mod_full_path, where you can define the full path to the shibboleth module, if os=rhel || centos

--- manifests/mod/shib.pp
+++ manifests/mod/shib.pp
@@ -1,15 +1,24 @@
 class apache::mod::shib (
   $suppress_warning = false,
+  $mod_full_path = undef,
 ) {
-
-  if $::osfamily == 'RedHat' and ! $suppress_warning {
-    warning('RedHat distributions do not have Apache mod_shib in their default package repositories.')
-  }
-
   $mod_shib = 'shib2'
 
-  apache::mod {$mod_shib:
-    id => 'mod_shib',
+  if ($::osfamily == 'RedHat' or $::osfamily == 'CentOS') and ! $suppress_warning {
+    # RedHat distributions do not have Apache mod_shib in their default package repositories
+    # use mod_full_path to set the full path for the shibboleth module
+    if ($mod_full_path == undef) {
+      fail("apache::mod::shib os=$::osfamily mod_full_path must be specified!")
+    }
+    ::apache::mod { $mod_shib:
+      id   => 'mod_shib',
+      path => "${mod_full_path}",
+    }
   }
-
+  else {
+    apache::mod {$mod_shib:
+      id   => 'mod_shib',
+    }
+  }
 }
+
