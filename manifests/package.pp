61cb21a9e8d2979e8e11131fa651f4732bc920fc
Romain Tartière <romain@blogreen.org>
2016-07-07 12:02:06 +0200

    MPM backend are dynamic on FreeBSD since 2015.
    This makes it easy to switch MPM.
    https://svnweb.freebsd.org/ports?view=revision&revision=378215

--- manifests/package.pp
+++ manifests/package.pp
@@ -12,20 +12,12 @@ class apache::package (
     'FreeBSD': {
       case $mpm_module {
         'prefork': {
-          $set = 'MPM_PREFORK'
-          $unset = 'MPM_WORKER MPM_EVENT'
         }
         'worker': {
-          $set = 'MPM_WORKER'
-          $unset = 'MPM_PREFORK MPM_EVENT'
         }
         'event': {
-          $set = 'MPM_EVENT'
-          $unset = 'MPM_PREFORK MPM_WORKER'
         }
         'itk': {
-          $set = undef
-          $unset = undef
           package { 'www/mod_mpm_itk':
             ensure => installed,
           }
@@ -33,23 +25,6 @@ class apache::package (
         default: { fail("MPM module ${mpm_module} not supported on FreeBSD") }
       }
 
-      # Configure ports to have apache build options set correctly
-      if $set {
-        file_line { 'apache SET options in /etc/make.conf':
-          ensure => $ensure,
-          path   => '/etc/make.conf',
-          line   => "apache24_SET_FORCE=${set}",
-          match  => '^apache24_SET_FORCE=.*',
-          before => Package['httpd'],
-        }
-        file_line { 'apache UNSET options in /etc/make.conf':
-          ensure => $ensure,
-          path   => '/etc/make.conf',
-          line   => "apache24_UNSET_FORCE=${unset}",
-          match  => '^apache24_UNSET_FORCE=.*',
-          before => Package['httpd'],
-        }
-      }
       $apache_package = $::apache::apache_name
     }
     default: {
