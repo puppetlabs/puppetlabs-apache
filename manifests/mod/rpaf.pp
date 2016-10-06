49f8bf579ee2211da4a36bd9c93b4ce7c2a14a09
Guy Vandenbergh <gvdb1967@gmail.com>
2016-10-06 14:38:37 +0200

    Add rpaf.conf template parameter
    Enable usage of different rpaf.conf file (e.g. rpaf forks 0.6.1
    https://github.com/gnif/mod_rpaf)

--- manifests/mod/rpaf.pp
+++ manifests/mod/rpaf.pp
@@ -1,7 +1,8 @@
 class apache::mod::rpaf (
   $sethostname = true,
   $proxy_ips   = [ '127.0.0.1' ],
-  $header      = 'X-Forwarded-For'
+  $header      = 'X-Forwarded-For',
+  $template    = 'apache/mod/rpaf.conf.erb'
 ) {
   include ::apache
   ::apache::mod { 'rpaf': }
@@ -14,7 +15,7 @@ class apache::mod::rpaf (
     ensure  => file,
     path    => "${::apache::mod_dir}/rpaf.conf",
     mode    => $::apache::file_mode,
-    content => template('apache/mod/rpaf.conf.erb'),
+    content => template($template),
     require => Exec["mkdir ${::apache::mod_dir}"],
     before  => File[$::apache::mod_dir],
     notify  => Class['apache::service'],
