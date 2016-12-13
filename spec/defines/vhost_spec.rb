a265ce74b5b3c37006c87e15e0d6497ddb1812d9
Emerson Prado <emerson.prado@dataprev.gov.br>
2016-12-13 16:24:46 -0200

    Remove spec test for apache::vhost::rewrite_inherit parameter
    Found no way of making spec test work with file content included by
    Puppet template

--- spec/defines/vhost_spec.rb
+++ spec/defines/vhost_spec.rb
@@ -1110,22 +1110,6 @@ describe 'apache::vhost', :type => :define do
       let :facts do default_facts end
       it { is_expected.to compile }
     end
-    context 'inherit global rewrite rules' do
-      let :params do
-        {
-          'docroot'         => '/rspec/docroot',
-          'rewrites'        => [
-            {
-              'rewrite_rule' => ['^index\.html$ welcome.html']
-            }
-          ],
-          'rewrite_inherit' => true,
-        }
-      end
-      it { is_expected.to contain_concat__fragment('rspec.example.com-rewrite').with(
-        :content => /RewriteOptions Inherit/
-      )}
-    end
     context 'bad suexec_user_group' do
       let :params do
         {
