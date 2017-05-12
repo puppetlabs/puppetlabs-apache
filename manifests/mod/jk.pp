e3957dd71d644153185aecd7fac6f97f02a1913c
Emerson Prado <emerson.prado@dataprev.gov.br>
2017-05-12 15:59:19 -0300

    Include jk.conf file resource in class ...mod::jk

--- manifests/mod/jk.pp
+++ manifests/mod/jk.pp
@@ -4,4 +4,16 @@ class apache::mod::jk {
 
   ::apache::mod { 'jk': }
 
+  file {'jk.conf':
+    ensure  => file,
+    path    => "${::apache::mod_dir}/jk.conf",
+    mode    => $::apache::file_mode,
+    content => template('apache/mod/jk.conf.erb'),
+    require => [
+      Exec["mkdir ${::apache::mod_dir}"],
+      File[$::apache::mod_dir],
+    ],
+    notify  => Class['apache::service'],
+  }
+
 }
