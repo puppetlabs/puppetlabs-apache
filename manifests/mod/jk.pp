ca7abf1673b82811cb5a1cb02b0c6b1a34cc8c7f
Emerson Prado <emerson.prado@dataprev.gov.br>
2017-06-06 14:07:55 -0300

    Include remaining parameters in class ...mod::jk and template

--- manifests/mod/jk.pp
+++ manifests/mod/jk.pp
@@ -1,10 +1,50 @@
+# Class apache::mod::jk
+#
+# Manages mod_jk connector
+#
+# All parameters are optional. When undefined, some receive default values,
+# while others cause an optional directive to be absent
+#
+# For help on parameters, pls see official reference at:
+# https://tomcat.apache.org/connectors-doc/reference/apache.html
+#
 class apache::mod::jk (
-  $workers_file,
-  $jk_mount = undef,
-  $jk_mount_file = undef,
-  $log_file = undef,
-  $log_level = undef,
-  $log_stamp_format = undef,
+  $workers_file          = undef,
+  $worker_property       = undef,
+  $shm_file              = undef,
+  $shm_size              = undef,
+  $mount_file            = undef,
+  $mount_file_reload     = undef,
+  $mount                 = undef,
+  $un_mount              = undef,
+  $auto_alias            = undef,
+  $mount_copy            = undef,
+  $worker_indicator      = undef,
+  $watchdog_interval     = undef,
+  $log_file              = undef,
+  $log_level             = undef,
+  $log_stamp_format      = undef,
+  $request_log_format    = undef,
+  $extract_ssl           = undef,
+  $https_indicator       = undef,
+  $sslprotocol_indicator = undef,
+  $certs_indicator       = undef,
+  $cipher_indicator      = undef,
+  $certchain_prefix      = undef,
+  $session_indicator     = undef,
+  $keysize_indicator     = undef,
+  $local_name_indicator  = undef,
+  $ignore_cl_indicator   = undef,
+  $local_addr_indicator  = undef,
+  $local_port_indicator  = undef,
+  $remote_host_indicator = undef,
+  $remote_addr_indicator = undef,
+  $remote_port_indicator = undef,
+  $remote_user_indicator = undef,
+  $auth_type_indicator   = undef,
+  $options               = undef,
+  $env_var               = undef,
+  $strip_session         = undef,
 ){
 
   include ::apache
