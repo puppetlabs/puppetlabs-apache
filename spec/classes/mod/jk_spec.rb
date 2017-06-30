f1c0f2b1bc81fa8a0e74d67a96725c5c405088a5
Emerson Prado <emerson.prado@dataprev.gov.br>
2017-06-30 17:45:24 -0300

    Include basic test spec for apache::mod::jk

---
+++ spec/classes/mod/jk_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe 'apache::mod::jk', :type => :class do
+  it_behaves_like 'a mod class, without including apache'
+
+  it { is_expected.to compile }
+  it { is_expected.to create_class('apache::mod::jk') }
+  it { is_expected.to contain_class('apache') }
+  it { is_expected.to contain_apache__mod('jk') }
+  it { is_expected.to contain_file('jk.conf').that_notifies('Class[apache::service]').with(
+    :ensure  => file,
+    :content => /<IfModule jk_module>/),
+    :content => /<\/IfModule>/),
+  }
+
+end
