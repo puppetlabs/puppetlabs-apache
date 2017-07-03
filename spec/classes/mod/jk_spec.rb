725b54cad64c847ef7027ce5310273d1b275a0bf
Emerson Prado <emerson.prado@dataprev.gov.br>
2017-07-03 17:52:04 -0300

    Include shared context in apache::mod::jk spec test

--- spec/classes/mod/jk_spec.rb
+++ spec/classes/mod/jk_spec.rb
@@ -3,6 +3,18 @@ require 'spec_helper'
 describe 'apache::mod::jk', :type => :class do
   it_behaves_like 'a mod class, without including apache'
 
+  shared_examples 'minimal resources' do
+    it { is_expected.to compile }
+    it { is_expected.to create_class('apache::mod::jk') }
+    it { is_expected.to contain_class('apache') }
+    it { is_expected.to contain_apache__mod('jk') }
+    it { is_expected.to contain_file('jk.conf').that_notifies('Class[apache::service]').with(
+      :ensure  => 'file',
+      :content => /<IfModule jk_module>/,
+      :content => /<\/IfModule>/,
+    )}
+  end
+
   context "with only required facts and no parameters" do
 
     let :facts do
@@ -13,15 +25,7 @@ describe 'apache::mod::jk', :type => :class do
       }
     end
 
-    it { is_expected.to compile }
-    it { is_expected.to create_class('apache::mod::jk') }
-    it { is_expected.to contain_class('apache') }
-    it { is_expected.to contain_apache__mod('jk') }
-    it { is_expected.to contain_file('jk.conf').that_notifies('Class[apache::service]').with(
-      :ensure  => 'file',
-      :content => /<IfModule jk_module>/,
-      :content => /<\/IfModule>/,
-    )}
+    it_behaves_like 'minimal resources'
 
   end
 
