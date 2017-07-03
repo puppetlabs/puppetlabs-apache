d619dd71375e16da785811f32fa78b3b38232075
Emerson Prado <emerson.prado@dataprev.gov.br>
2017-07-03 15:50:06 -0300

    Include RHEL 6 context and facts required by included classes

--- spec/classes/mod/jk_spec.rb
+++ spec/classes/mod/jk_spec.rb
@@ -3,14 +3,26 @@ require 'spec_helper'
 describe 'apache::mod::jk', :type => :class do
   it_behaves_like 'a mod class, without including apache'
 
-  it { is_expected.to compile }
-  it { is_expected.to create_class('apache::mod::jk') }
-  it { is_expected.to contain_class('apache') }
-  it { is_expected.to contain_apache__mod('jk') }
-  it { is_expected.to contain_file('jk.conf').that_notifies('Class[apache::service]').with(
-    :ensure  => file,
-    :content => /<IfModule jk_module>/,
-    :content => /<\/IfModule>/,
-  )}
+  context "with only required facts and no parameters" do
+
+    let :facts do
+      {
+        :osfamily               => 'RedHat',
+        :operatingsystem        => 'RedHat',
+        :operatingsystemrelease => '6',
+      }
+    end
+
+    it { is_expected.to compile }
+    it { is_expected.to create_class('apache::mod::jk') }
+    it { is_expected.to contain_class('apache') }
+    it { is_expected.to contain_apache__mod('jk') }
+    it { is_expected.to contain_file('jk.conf').that_notifies('Class[apache::service]').with(
+      :ensure  => file,
+      :content => /<IfModule jk_module>/,
+      :content => /<\/IfModule>/,
+    )}
+
+  end
 
 end
