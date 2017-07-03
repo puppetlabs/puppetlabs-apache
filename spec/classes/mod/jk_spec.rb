1c2a3d84b9cdec7c254d8cc23b456c22bd42877b
Emerson Prado <emerson.prado@dataprev.gov.br>
2017-07-03 16:13:15 -0300

    Remove parameters from jk.conf file in mod::jk spec test

--- spec/classes/mod/jk_spec.rb
+++ spec/classes/mod/jk_spec.rb
@@ -17,11 +17,7 @@ describe 'apache::mod::jk', :type => :class do
     it { is_expected.to create_class('apache::mod::jk') }
     it { is_expected.to contain_class('apache') }
     it { is_expected.to contain_apache__mod('jk') }
-    it { is_expected.to contain_file('jk.conf').that_notifies('Class[apache::service]').with(
-      :ensure  => file,
-      :content => /<IfModule jk_module>/,
-      :content => /<\/IfModule>/,
-    )}
+    it { is_expected.to contain_file('jk.conf') }
 
   end
 
