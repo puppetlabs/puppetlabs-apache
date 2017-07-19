f1c37edd5f60dcd80542bfd4c5df09fc4e9bc91d
Hunter Haugen <hunter@puppet.com>
2017-07-19 11:06:09 -0700

    (MODULES-4933) Allow custom UserDir string

--- manifests/mod/userdir.pp
+++ manifests/mod/userdir.pp
@@ -1,13 +1,29 @@
 class apache::mod::userdir (
-  $home = '/home',
-  $dir = 'public_html',
+  $home = undef,
+  $dir = undef,
   $disable_root = true,
   $apache_version = undef,
+  $path = '/home/*/public_html',
   $overrides = [ 'FileInfo', 'AuthConfig', 'Limit', 'Indexes' ],
   $options = [ 'MultiViews', 'Indexes', 'SymLinksIfOwnerMatch', 'IncludesNoExec' ],
 ) {
   include ::apache
   $_apache_version = pick($apache_version, $apache::apache_version)
+
+  if $home or $dir {
+    $_home = $home ? {
+      undef   => '/home',
+      default =>  $home,
+    }
+    $_dir = $dir ? {
+      undef   => 'public_html',
+      default =>  $dir,
+    }
+    warning('home and dir are deprecated; use path instead')
+    $_path = "${_home}/*/${_dir}"
+  } else {
+    $_path = $path
+  }
 
   ::apache::mod { 'userdir': }
 
