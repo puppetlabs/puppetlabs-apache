f660d5450c4846461213540a944103c524e056e4
Hunter Haugen <hunter@puppet.com>
2017-10-06 16:55:52 -0700

    Move apply_manifest into rspec 'it'

--- spec/acceptance/init_task_spec.rb
+++ spec/acceptance/init_task_spec.rb
@@ -3,16 +3,16 @@ require 'spec_helper_acceptance'
 
 describe 'apache tasks', if: pe_install? && puppet_version =~ %r{(5\.\d\.\d)} do
   describe 'reload' do
-    pp = <<-EOS
-    class { 'apache':
-      default_vhost => false,
-    }
-    apache::listen { '9090':}
-    EOS
-
-    apply_manifest(pp, :catch_failures => true)
-
     it 'execute reload' do
+      pp = <<-EOS
+      class { 'apache':
+        default_vhost => false,
+      }
+      apache::listen { '9090':}
+      EOS
+
+      apply_manifest(pp, :catch_failures => true)
+
       result = run_task(task_name: 'apache', params: 'action=reload')
       expect_multiple_regexes(result: result, regexes: [%r{reload successful}, %r{Job completed. 1/1 nodes succeeded}])
     end
