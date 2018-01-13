fac74ef53e06a790253410362fa139743dfdc842
juju4 <juju4@users.noreply.github.com>
2018-01-13 13:17:29 -0500

    security.txt configuration - https://securitytxt.org/

--- manifests/params.pp
+++ manifests/params.pp
@@ -150,6 +150,8 @@ class apache::params inherits ::apache::version {
       'nss' => 'libmodnss.so',
     }
     $conf_template        = 'apache/httpd.conf.erb'
+    $security_txt         = true
+    $security_txt_template  = 'apache/vhost/security.txt.erb'
     $http_protocol_options  = undef
     $keepalive            = 'On'
     $keepalive_timeout    = 15
@@ -301,6 +303,8 @@ class apache::params inherits ::apache::version {
       'shib2' => $shib2_lib,
     }
     $conf_template          = 'apache/httpd.conf.erb'
+    $security_txt           = true
+    $security_txt_template  = 'apache/vhost/security.txt.erb'
     $http_protocol_options  = undef
     $keepalive              = 'On'
     $keepalive_timeout      = 15
@@ -426,6 +430,8 @@ class apache::params inherits ::apache::version {
     $mod_libs         = {
     }
     $conf_template        = 'apache/httpd.conf.erb'
+    $security_txt         = true
+    $security_txt_template = 'apache/vhost/security.txt.erb'
     $http_protocol_options = undef
     $keepalive            = 'On'
     $keepalive_timeout    = 15
@@ -493,6 +499,8 @@ class apache::params inherits ::apache::version {
     $mod_libs         = {
     }
     $conf_template        = 'apache/httpd.conf.erb'
+    $security_txt         = true
+    $security_txt_template = 'apache/vhost/security.txt.erb'
     $http_protocol_options = undef
     $keepalive            = 'On'
     $keepalive_timeout    = 15
@@ -559,6 +567,8 @@ class apache::params inherits ::apache::version {
       'php53'          => '/usr/lib64/apache2/mod_php5.so',
     }
     $conf_template          = 'apache/httpd.conf.erb'
+    $security_txt           = true
+    $security_txt_template  = 'apache/vhost/security.txt.erb'
     $http_protocol_options  = undef
     $keepalive              = 'On'
     $keepalive_timeout      = 15
