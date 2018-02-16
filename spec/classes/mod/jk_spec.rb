3384c57eedff4e7a92285c1daa9df9ea43a09dc9
Ryan Hall <ryan_hall@glic.com>
2018-02-16 00:47:10 -0500

    fix hash variable

--- spec/classes/mod/jk_spec.rb
+++ spec/classes/mod/jk_spec.rb
@@ -197,7 +197,7 @@ describe 'apache::mod::jk', type: :class do
     it { is_expected.not_to contain_apache__listen("#{default_ip}:#{default_port}") }
   end
 
-  [
+  validation = {
     default: {
       shm_file: :undef,
       log_file: :undef,
@@ -222,7 +222,8 @@ describe 'apache::mod::jk', type: :class do
       shm_path: '/var/log/httpd/jk-runtime-status',
       log_path: '"|rotatelogs /var/log/httpd/mod_jk.log.%Y%m%d 86400 -180"',
     },
-  ].each do |option, paths|
+  }
+  validation.each do |option, paths|
     context "RHEL 6 with #{option} shm_file and log_file paths" do
       let :facts do
         {
