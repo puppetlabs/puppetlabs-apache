191528901a7f30ffecfd71dd5daf9003d5b00949
Helen Campbell <helen@puppet.com>
2018-07-10 11:59:51 +0100

    [MODULES-7428] Refactor all apache facts and functions to only be 2.4 compatible

--- spec/classes/mod/itk_spec.rb
+++ spec/classes/mod/itk_spec.rb
@@ -24,29 +24,6 @@ describe 'apache::mod::itk', type: :class do
     it { is_expected.to contain_file('/etc/apache2/mods-available/itk.conf').with_ensure('file') }
     it { is_expected.to contain_file('/etc/apache2/mods-enabled/itk.conf').with_ensure('link') }
 
-    # context 'with Apache version < 2.4' do
-    #   let :params do
-    #     {
-    #       apache_version: '2.2',
-    #     }
-    #   end
-
-    #   it { is_expected.not_to contain_file('/etc/apache2/mods-available/itk.load') }
-    #   it { is_expected.not_to contain_file('/etc/apache2/mods-enabled/itk.load') }
-
-    #   it { is_expected.to contain_package('apache2-mpm-itk') }
-    # end
-    # context 'with Apache version < 2.4 with enablecapabilities set' do
-    #   let :params do
-    #     {
-    #       apache_version: '2.2',
-    #       enablecapabilities: true,
-    #     }
-    #   end
-
-    #   it { is_expected.not_to contain_file('/etc/apache2/mods-available/itk.conf').with_content(%r{EnableCapabilities}) }
-    # end
-
     context 'with Apache version >= 2.4' do
       let :pre_condition do
         'class { "apache": mpm_module => prefork, }'
@@ -95,28 +72,6 @@ describe 'apache::mod::itk', type: :class do
     it { is_expected.not_to contain_apache__mod('itk') }
     it { is_expected.to contain_file('/etc/httpd/conf.modules.d/itk.conf').with_ensure('file') }
     it { is_expected.to contain_package('httpd-itk') }
-
-    # context 'with Apache version < 2.4' do
-    #   let :params do
-    #     {
-    #       apache_version: '2.2',
-    #     }
-    #   end
-
-    #   it {
-    #     is_expected.to contain_file_line('/etc/sysconfig/httpd itk enable').with('require' => 'Package[httpd]')
-    #   }
-    # end
-    # context 'with Apache version < 2.4 with enablecapabilities set' do
-    #   let :params do
-    #     {
-    #       apache_version: '2.2',
-    #       enablecapabilities: 'On',
-    #     }
-    #   end
-
-    #   it { is_expected.not_to contain_file('/etc/httpd/conf.modules.d/itk.conf').with_content(%r{EnableCapabilities}) }
-    # end
 
     context 'with Apache version >= 2.4' do
       let :pre_condition do
