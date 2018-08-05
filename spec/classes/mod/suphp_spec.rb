b780d6558d1ae5956263cc5076f3dfa3d122b0c3
Tim Meusel <tim@bastelfreak.de>
2018-08-05 19:59:46 +0200

    migrate suphp spec tests to rspec-puppet-facts

--- spec/classes/mod/suphp_spec.rb
+++ spec/classes/mod/suphp_spec.rb
@@ -1,38 +1,26 @@
 require 'spec_helper'
 
 describe 'apache::mod::suphp', type: :class do
-  it_behaves_like 'a mod class, without including apache'
-  context 'on a Debian OS' do
-    let :facts do
-      {
-        osfamily: 'Debian',
-        operatingsystemrelease: '8',
-        lsbdistcodename: 'jessie',
-        operatingsystem: 'Debian',
-        id: 'root',
-        kernel: 'Linux',
-        path: '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-        is_pe: false,
-      }
-    end
+  on_supported_os.each do |os, facts|
+    context "on #{os} " do
+      let :facts do
+        facts
+      end
 
-    it { is_expected.to contain_class('apache::params') }
-    it { is_expected.to contain_package('libapache2-mod-suphp') }
-  end
-  context 'on a RedHat OS' do
-    let :facts do
-      {
-        osfamily: 'RedHat',
-        operatingsystemrelease: '6',
-        operatingsystem: 'RedHat',
-        id: 'root',
-        kernel: 'Linux',
-        path: '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-        is_pe: false,
-      }
+      case facts[:os]['family']
+      when 'Debian'
+        if facts[:os]['release']['major'].to_i < 18
+          context 'on a Debian OS' do
+            it { is_expected.to contain_class('apache::params') }
+            it { is_expected.to contain_package('libapache2-mod-suphp') }
+          end
+        end
+      when 'RedHat'
+        context 'on a RedHat OS' do
+          it { is_expected.to contain_class('apache::params') }
+          it { is_expected.to contain_package('mod_suphp') }
+        end
+      end
     end
-
-    it { is_expected.to contain_class('apache::params') }
-    it { is_expected.to contain_package('mod_suphp') }
   end
 end
