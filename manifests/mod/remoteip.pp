f496e8eb12610a139c95de117e2089b323cf8e56
Johan Fleury <jfleury@arcaik.net>
2019-02-02 17:14:55 -0500

    feat(mod::remoteip): manage all parameters supported by Apache

--- manifests/mod/remoteip.pp
+++ manifests/mod/remoteip.pp
@@ -14,10 +14,23 @@
 # @param proxy_ips
 #   *Deprecated*: use `$internal_proxy` instead.
 #
+# @param internal_proxy_list
+#   The path to a file containing a list of IP addresses, IP blocks or hostname
+#   that are trusted to set a valid value inside the specified header. See
+#   `$internal_proxy` for details.
+#
 # @param proxies_header
 #   A header into which `mod_remoteip` will collect a list of all of the
 #   intermediate client IP addresses trusted to resolve the useragent IP of the
 #   request (e.g. `X-Forwarded-By`).
+#
+# @param proxy_protocol
+#   Wether or not to enable the PROXY protocol header handling. If enabled
+#   upstream clients must set the header every time they open a connection.
+#
+# @param proxy_protocol_exceptions
+#   A list of IP address or IP blocks that are not required to use the PROXY
+#   protocol.
 #
 # @param trusted_proxy
 #   A list of IP addresses, IP blocks or hostname that are trusted to set a
@@ -28,20 +41,30 @@
 # @param trusted_proxy_ips
 #   *Deprecated*: use `$trusted_proxy` instead.
 #
+# @param trusted_proxy_list
+#   The path to a file containing a list of IP addresses, IP blocks or hostname
+#   that are trusted to set a valid value inside the specified header. See
+#   `$trusted_proxy` for details.
+#
 # @param apache_version
 #   A version string used to validate that your apache version supports
 #   `mod_remoteip`. If not specified, `$::apache::apache_version` is used.
 #
 class apache::mod::remoteip (
-  String                        $header            = 'X-Forwarded-For',
-  Optional[Array[Stdlib::Host]] $internal_proxy    = undef,
-  Optional[Array[Stdlib::Host]] $proxy_ips         = undef,
-  Optional[String]              $proxies_header    = undef,
-  Optional[Array[Stdlib::Host]] $trusted_proxy     = undef,
-  Optional[Array[Stdlib::Host]] $trusted_proxy_ips = undef,
-  Optional[String]              $apache_version    = undef,
+  String                         $header                    = 'X-Forwarded-For',
+  Optional[Array[Stdlib::Host]]  $internal_proxy            = undef,
+  Optional[Array[Stdlib::Host]]  $proxy_ips                 = undef,
+  Optional[Stdlib::Absolutepath] $internal_proxy_list       = undef,
+  Optional[String]               $proxies_header            = undef,
+  Boolean                        $proxy_protocol            = false,
+  Optional[Array[Stdlib::Host]]  $proxy_protocol_exceptions = undef,
+  Optional[Array[Stdlib::Host]]  $trusted_proxy             = undef,
+  Optional[Array[Stdlib::Host]]  $trusted_proxy_ips         = undef,
+  Optional[Stdlib::Absolutepath] $trusted_proxy_list        = undef,
+  Optional[String]               $apache_version            = undef,
 ) {
   include ::apache
+
   $_apache_version = pick($apache_version, $apache::apache_version)
   if versioncmp($_apache_version, '2.4') < 0 {
     fail('mod_remoteip is only available in Apache 2.4')
@@ -68,8 +91,12 @@ class apache::mod::remoteip (
   $template_parameters = {
     header                    => $header,
     internal_proxy            => $_internal_proxy,
+    internal_proxy_list       => $internal_proxy_list,
     proxies_header            => $proxies_header,
+    proxy_protocol            => $proxy_protocol,
+    proxy_protocol_exceptions => $proxy_protocol_exceptions,
     trusted_proxy             => $_trusted_proxy,
+    trusted_proxy_list        => $trusted_proxy_list,
   }
 
   file { 'remoteip.conf':
