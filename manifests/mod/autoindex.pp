b5c8963e6ccb2e1f2ebc389f83567ce40bdc61eb
2and3makes23 <59733711+2and3makes23@users.noreply.github.com>
2020-01-23 12:18:47 +0100

    Less if-statements, better readability

--- manifests/mod/autoindex.pp
+++ manifests/mod/autoindex.pp
@@ -6,7 +6,17 @@
 class apache::mod::autoindex {
   include ::apache
   ::apache::mod { 'autoindex': }
-  # Template changes content based on @osfamily
+
+  # Determine icon filename suffix for autoindex.conf.erb
+  case $operatingsystem {
+    'Debian', 'Ubuntu': {
+      icon_suffix = '-20x22'
+    }
+    default: {
+      icon_suffix = ''
+    }
+  }
+
   file { 'autoindex.conf':
     ensure  => file,
     path    => "${::apache::mod_dir}/autoindex.conf",
