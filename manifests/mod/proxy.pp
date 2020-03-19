dbb8767b91713e2ee4d1fa2412796a40dda2a738
Jon-Paul Lindquist <jlindquist@godaddy.com>
2020-03-18 17:42:26 -0700

    Add ProxyIOBufferSize option

--- manifests/mod/proxy.pp
+++ manifests/mod/proxy.pp
@@ -1,33 +1,34 @@
 # @summary
 #   Installs and configures `mod_proxy`.
-# 
+#
 # @param proxy_requests
 #   Enables forward (standard) proxy requests.
-# 
+#
 # @param allow_from
 #   List of IPs allowed to access proxy.
-# 
+#
 # @param apache_version
 #   Used to verify that the Apache version you have requested is compatible with the module.
-# 
+#
 # @param package_name
 #   Name of the proxy package to install.
-# 
+#
 # @param proxy_via
 #   Set local IP address for outgoing proxy connections.
-# 
+#
 # @param proxy_timeout
 #   Network timeout for proxied requests.
-# 
+#
 # @see https://httpd.apache.org/docs/current/mod/mod_proxy.html for additional documentation.
 #
 class apache::mod::proxy (
-  $proxy_requests = 'Off',
-  $allow_from     = undef,
-  $apache_version = undef,
-  $package_name   = undef,
-  $proxy_via      = 'On',
-  $proxy_timeout  = undef,
+  $proxy_requests     = 'Off',
+  $allow_from         = undef,
+  $apache_version     = undef,
+  $package_name       = undef,
+  $proxy_via          = 'On',
+  $proxy_timeout      = undef,
+  $proxyiobuffersize  = undef,
 ) {
   include ::apache
   $_proxy_timeout = $apache::timeout
