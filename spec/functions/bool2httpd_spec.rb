331e5273a655ed594456bb2971f3628e56ae139c
Alexander Fisher <alex@linfratech.co.uk>
2020-05-02 11:39:18 +0100

    Replace legacy `bool2httpd()` function with shim
    This commit replaces the legacy
    `lib/puppet/parser/functions/bool2httpd.rb` function with a 4.x API shim
    function that calls the namespaced `apache::bool2httpd` function.
    
    The shim might not actually be necessary as the function is probably
    `private`.
    
    The non-namespaced legacy version was still being used in a few
    instances in templates.  This has been corrected.

--- spec/functions/bool2httpd_spec.rb
+++ spec/functions/bool2httpd_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'apache::bool2httpd' do
+shared_examples 'apache::bool2httpd function' do
   it { is_expected.not_to eq(nil) }
   it { is_expected.to run.with_params.and_raise_error(ArgumentError) }
   it { is_expected.to run.with_params('1', '2').and_raise_error(ArgumentError) }
@@ -16,4 +16,14 @@ describe 'apache::bool2httpd' do
   it { is_expected.to run.with_params(nil).and_return('Off') }
   it { is_expected.to run.with_params(:undef).and_return('Off') }
   it { is_expected.to run.with_params('foo').and_return('foo') }
+end
+
+describe 'apache::bool2httpd' do
+  it_behaves_like 'apache::bool2httpd function'
+
+  describe 'deprecated non-namespaced shim' do
+    describe 'bool2httpd', type: :puppet_function do
+      it_behaves_like 'apache::bool2httpd function'
+    end
+  end
 end
