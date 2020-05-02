4016e418ab8c7040f28b90df694550e0f1986f5e
Alexander Fisher <alex@linfratech.co.uk>
2020-05-02 11:05:38 +0100

    Tidy up `pw_hash` function
    * Remove legacy non-namespaced `lib/puppet/parser/functions/apache_pw_hash.rb`
    * Create 4x API non-namespaced `apache_pw_hash` shim that calls `apache::pw_hash`
    * Rename existing 4x API `apache::apache_pw_hash` to `apache::pw_hash`
    * Create shim for `apache::apache_pw_hash` that calls `apache::pw_hash`
    * Mark shims as DEPRECATED
    * Test all 3 versions work

---
+++ lib/puppet/functions/apache_pw_hash.rb
@@ -0,0 +1,10 @@
+# @summary DEPRECATED.  Use the namespaced function [`apache::pw_hash`](#apachepw_hash) instead.
+Puppet::Functions.create_function(:apache_pw_hash) do
+  dispatch :deprecation_gen do
+    repeated_param 'Any', :args
+  end
+  def deprecation_gen(*args)
+    call_function('deprecation', 'apache_pw_hash', 'This function is deprecated, please use apache::pw_hash instead.')
+    call_function('apache::pw_hash', *args)
+  end
+end
