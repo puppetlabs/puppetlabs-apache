2e24cc3ef6db70d85acc54a3b6ae845c73b5122c
Minor, Thomas <Thomas.Minor@kps.com>
2020-12-04 10:55:03 +0100

    added missing configuration parameters to mod disk_cache

--- manifests/mod/disk_cache.pp
+++ manifests/mod/disk_cache.pp
@@ -12,14 +12,52 @@
 # @param cache_ignore_headers
 #   Specifies HTTP header(s) that should not be stored in the cache.
 #
+# @param cache_dir_length
+#   The number of characters in subdirectory names
+#
+# @param cache_dir_levels
+#   The number of levels of subdirectories in the cache.
+#
+# @param cache_default_expire
+#   The default duration to cache a document when no expiry date is specified.
+#
+# @param cache_max_expire
+#   The maximum time in seconds to cache a document
+#
+# @param cache_ignore_no_lastmod
+#   Ignore the fact that a response has no Last Modified header.
+#
+# @param cache_header
+#   Add an X-Cache header to the response.
+#
+# @param cache_lock
+#   Enable the thundering herd lock.
+#
+# @param cache_ignore_cache_control
+#   Ignore request to not serve cached content to client
+#
+# @param cache_max_filesize
+#   The maximum size (in bytes) of a document to be placed in the cache
+#
 # @note
 #   Apache 2.2, mod_disk_cache installed. On Apache 2.4, mod_cache_disk installed.
 #
-# @see https://httpd.apache.org/docs/2.2/mod/mod_disk_cache.html for additional documentation.
+# @see https://httpd.apache.org/docs/2.2/mod/mod_disk_cache.html for additional documentation on version 2.2.
+#
+# @see https://httpd.apache.org/docs/2.4/mod/mod_cache_disk.html for additional documentation on version 2.4.
 #
 class apache::mod::disk_cache (
-  $cache_root           = undef,
-  $cache_ignore_headers = undef,
+  $cache_root                 = undef,
+  $cache_ignore_headers       = undef,
+  $cache_dir_length           = undef,
+  $cache_dir_levels           = undef,
+  $cache_default_expire       = undef,
+  $cache_max_expire           = undef,
+  $cache_ignore_no_lastmod    = undef,
+  $cache_header               = undef,
+  $cache_lock                 = undef,
+  $cache_ignore_cache_control = undef,
+  $cache_max_filesize         = undef,
 ) {
   include apache
   if $cache_root {
@@ -49,8 +87,9 @@ class apache::mod::disk_cache (
 
   Class['::apache::mod::cache'] -> Class['::apache::mod::disk_cache']
 
-  # Template uses $_cache_root
+  # Template uses $_cache_root and others
   file { 'disk_cache.conf':
+
     ensure  => file,
     path    => "${apache::mod_dir}/disk_cache.conf",
     mode    => $apache::file_mode,
