b57883ee95d1bb1fdfc918dee0ddf15773816236
Minor, Thomas <thomas.minor@kps.com>
2020-12-08 13:22:51 +0100

    Added cache_disk
    Deprecates disk_cache

--- manifests/mod/disk_cache.pp
+++ manifests/mod/disk_cache.pp
@@ -6,55 +6,29 @@
 #   Default depends on the Apache version and operating system:
 #   - Debian: /var/cache/apache2/mod_cache_disk
 #   - FreeBSD: /var/cache/mod_cache_disk
-#   - Red Hat: /var/cache/httpd/proxy
+#   - Red Hat, Apache 2.4: /var/cache/httpd/proxy
+#   - Red Hat, Apache 2.2: /var/cache/mod_proxy
 #
 # @param cache_ignore_headers
 #   Specifies HTTP header(s) that should not be stored in the cache.
 #
-# @param default_cache_enable
-#   Default value is true, which enables "CacheEnable disk /" in disk_cache.conf for the webserver. This would cache
-#   every request to apache by default for every vhost. If set to false the default cache all behaviour is supressed.
-#   You can then control this behaviour in individual vhosts by explicitly defining CacheEnable.
-#
 # @note
-#   On Apache 2.4, mod_cache_disk installed.
+#   Apache 2.2, mod_disk_cache installed. On Apache 2.4, mod_cache_disk installed.
+#   This class is deprecated, use mode_cache_disk instead
 #
-# @see https://httpd.apache.org/docs/2.4/mod/mod_cache_disk.html for additional documentation.
+# @see https://httpd.apache.org/docs/2.2/mod/mod_disk_cache.html for additional documentation on version 2.2.
+#
+# @see https://httpd.apache.org/docs/2.4/mod/mod_cache_disk.html for additional documentation on version 2.4.
 #
 class apache::mod::disk_cache (
-  Optional[Stdlib::Absolutepath] $cache_root = undef,
-  Optional[String] $cache_ignore_headers     = undef,
-  Boolean $default_cache_enable              = true,
+  $cache_root                 = undef,
+  $cache_ignore_headers       = undef,
 ) {
-  include apache
-  if $cache_root {
-    $_cache_root = $cache_root
-  } else {
-    $_cache_root = $facts['os']['family'] ? {
-      'Debian'  => '/var/cache/apache2/mod_cache_disk',
-      'RedHat'  => '/var/cache/httpd/proxy',
-      'FreeBSD' => '/var/cache/mod_cache_disk',
-    }
-  }
+  deprecation('apache::mod::disk_cache','This module is deprecated; please use apache::mod::cache_disk')
 
-  apache::mod { 'cache_disk': }
-
-  Class['apache::mod::cache'] -> Class['apache::mod::disk_cache']
-
-  $parameters = {
-    'default_cache_enable'  => $default_cache_enable,
-    '_cache_root'           => $_cache_root,
-    'cache_ignore_headers'  => $cache_ignore_headers,
-  }
-
-  # Template uses $_cache_root
-  file { 'disk_cache.conf':
-    ensure  => file,
-    path    => "${apache::mod_dir}/disk_cache.conf",
-    mode    => $apache::file_mode,
-    content => epp('apache/mod/disk_cache.conf.epp', $parameters),
-    require => Exec["mkdir ${apache::mod_dir}"],
-    before  => File[$apache::mod_dir],
-    notify  => Class['apache::service'],
+  class { 'apache::mod::cache_disk':
+    cache_root                 => $cache_root,
+    cache_enable              => ['/'],
+    cache_ignore_headers       => $cache_ignore_headers,
   }
 }
