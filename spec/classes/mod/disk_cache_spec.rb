44e29bb42b38a1363f917d6af9a53b8444338581
Pawan Pawar <pawanpawar43@gmail.com>
2021-05-03 13:57:38 +0200

    Spec tests for default_cache_enable behaviour.

--- spec/classes/mod/disk_cache_spec.rb
+++ spec/classes/mod/disk_cache_spec.rb
@@ -47,6 +47,66 @@ describe 'apache::mod::disk_cache', type: :class do
           .with(content: %r{CacheEnable disk \/\nCacheRoot \"\/var\/cache\/apache2\/mod_cache_disk\"\nCacheDirLevels 2\nCacheDirLength 1\nCacheIgnoreHeaders Set-Cookie})
       }
     end
+    context 'with $default_cache_enable = false' do
+      let :pre_condition do
+        'class{ "apache":
+          apache_version => "2.4",
+          default_mods   => ["cache"],
+          mod_dir        => "/tmp/junk",
+         }'
+      end
+
+      let(:params) {{ 'default_cache_enable' => false}}
+
+      it { is_expected.to compile }
+      it { is_expected.to contain_class('apache::mod::disk_cache') }
+      it { is_expected.to contain_class('apache::mod::cache').that_comes_before('Class[Apache::Mod::Disk_cache]') }
+      it { is_expected.to contain_apache_mod('cache_disk') }
+      it {
+        is_expected.to contain_file('disk_cache.conf')
+          .with(content: %r{CacheRoot \"\/var\/cache\/apache2\/mod_cache_disk\"\nCacheDirLevels 2\nCacheDirLength 1\nCacheIgnoreHeaders Set-Cookie})
+      }
+    end
+    context 'with $default_cache_enable = true' do
+      let :pre_condition do
+        'class{ "apache":
+          apache_version => "2.4",
+          default_mods   => ["cache"],
+          mod_dir        => "/tmp/junk",
+         }'
+      end
+
+      let(:params) {{ 'default_cache_enable' => true}}
+
+      it { is_expected.to compile }
+      it { is_expected.to contain_class('apache::mod::disk_cache') }
+      it { is_expected.to contain_class('apache::mod::cache').that_comes_before('Class[Apache::Mod::Disk_cache]') }
+      it { is_expected.to contain_apache_mod('cache_disk') }
+      it {
+        is_expected.to contain_file('disk_cache.conf')
+          .with(content: %r{CacheEnable disk \/\nCacheRoot \"\/var\/cache\/apache2\/mod_cache_disk\"\nCacheDirLevels 2\nCacheDirLength 1\nCacheIgnoreHeaders Set-Cookie})
+      }
+    end
+    context 'with $default_cache_enable = foo' do
+      let :pre_condition do
+        'class{ "apache":
+          apache_version => "2.4",
+          default_mods   => ["cache"],
+          mod_dir        => "/tmp/junk",
+         }'
+      end
+
+      let(:params) {{ 'default_cache_enable' => 'foo'}}
+
+      it { is_expected.to compile }
+      it { is_expected.to contain_class('apache::mod::disk_cache') }
+      it { is_expected.to contain_class('apache::mod::cache').that_comes_before('Class[Apache::Mod::Disk_cache]') }
+      it { is_expected.to contain_apache_mod('cache_disk') }
+      it {
+        is_expected.to contain_file('disk_cache.conf')
+          .with(content: %r{CacheEnable disk \/\nCacheRoot \"\/var\/cache\/apache2\/mod_cache_disk\"\nCacheDirLevels 2\nCacheDirLength 1\nCacheIgnoreHeaders Set-Cookie})
+      }.to raise_error(Puppet::Error, /default_cache_enable must be true or false/)
+    end
   end
 
   context 'on a RedHat 6-based OS' do
@@ -87,6 +147,66 @@ describe 'apache::mod::disk_cache', type: :class do
         is_expected.to contain_file('disk_cache.conf')
           .with(content: %r{CacheEnable disk \/\nCacheRoot \"\/var\/cache\/httpd\/proxy\"\nCacheDirLevels 2\nCacheDirLength 1\nCacheIgnoreHeaders Set-Cookie})
       }
+    end
+    context 'with $default_cache_enable = false' do
+      let :pre_condition do
+        'class{ "apache":
+          apache_version => "2.4",
+          default_mods   => ["cache"],
+          mod_dir        => "/tmp/junk",
+         }'
+      end
+
+      let(:params) {{ 'default_cache_enable' => false}}
+
+      it { is_expected.to compile }
+      it { is_expected.to contain_class('apache::mod::disk_cache') }
+      it { is_expected.to contain_class('apache::mod::cache').that_comes_before('Class[Apache::Mod::Disk_cache]') }
+      it { is_expected.to contain_apache_mod('cache_disk') }
+      it {
+        is_expected.to contain_file('disk_cache.conf')
+          .with(content: %r{CacheRoot \"\/var\/cache\/apache2\/mod_cache_disk\"\nCacheDirLevels 2\nCacheDirLength 1\nCacheIgnoreHeaders Set-Cookie})
+      }
+    end
+    context 'with $default_cache_enable = true' do
+      let :pre_condition do
+        'class{ "apache":
+          apache_version => "2.4",
+          default_mods   => ["cache"],
+          mod_dir        => "/tmp/junk",
+         }'
+      end
+
+      let(:params) {{ 'default_cache_enable' => true}}
+
+      it { is_expected.to compile }
+      it { is_expected.to contain_class('apache::mod::disk_cache') }
+      it { is_expected.to contain_class('apache::mod::cache').that_comes_before('Class[Apache::Mod::Disk_cache]') }
+      it { is_expected.to contain_apache_mod('cache_disk') }
+      it {
+        is_expected.to contain_file('disk_cache.conf')
+          .with(content: %r{CacheEnable disk \/\nCacheRoot \"\/var\/cache\/apache2\/mod_cache_disk\"\nCacheDirLevels 2\nCacheDirLength 1\nCacheIgnoreHeaders Set-Cookie})
+      }
+    end
+    context 'with $default_cache_enable = foo' do
+      let :pre_condition do
+        'class{ "apache":
+          apache_version => "2.4",
+          default_mods   => ["cache"],
+          mod_dir        => "/tmp/junk",
+         }'
+      end
+
+      let(:params) {{ 'default_cache_enable' => 'foo'}}
+
+      it { is_expected.to compile }
+      it { is_expected.to contain_class('apache::mod::disk_cache') }
+      it { is_expected.to contain_class('apache::mod::cache').that_comes_before('Class[Apache::Mod::Disk_cache]') }
+      it { is_expected.to contain_apache_mod('cache_disk') }
+      it {
+        is_expected.to contain_file('disk_cache.conf')
+          .with(content: %r{CacheEnable disk \/\nCacheRoot \"\/var\/cache\/apache2\/mod_cache_disk\"\nCacheDirLevels 2\nCacheDirLength 1\nCacheIgnoreHeaders Set-Cookie})
+      }.to raise_error(Puppet::Error, /default_cache_enable must be true or false/)
     end
   end
   context 'on a FreeBSD OS' do
@@ -133,6 +253,66 @@ describe 'apache::mod::disk_cache', type: :class do
         is_expected.to contain_file('disk_cache.conf')
           .with(content: %r{CacheEnable disk \/\nCacheRoot \"\/var\/cache\/mod_cache_disk\"\nCacheDirLevels 2\nCacheDirLength 1\nCacheIgnoreHeaders Set-Cookie})
       }
+    end
+    context 'with $default_cache_enable = false' do
+      let :pre_condition do
+        'class{ "apache":
+          apache_version => "2.4",
+          default_mods   => ["cache"],
+          mod_dir        => "/tmp/junk",
+         }'
+      end
+
+      let(:params) {{ 'default_cache_enable' => false}}
+
+      it { is_expected.to compile }
+      it { is_expected.to contain_class('apache::mod::disk_cache') }
+      it { is_expected.to contain_class('apache::mod::cache').that_comes_before('Class[Apache::Mod::Disk_cache]') }
+      it { is_expected.to contain_apache_mod('cache_disk') }
+      it {
+        is_expected.to contain_file('disk_cache.conf')
+          .with(content: %r{CacheRoot \"\/var\/cache\/apache2\/mod_cache_disk\"\nCacheDirLevels 2\nCacheDirLength 1\nCacheIgnoreHeaders Set-Cookie})
+      }
+    end
+    context 'with $default_cache_enable = true' do
+      let :pre_condition do
+        'class{ "apache":
+          apache_version => "2.4",
+          default_mods   => ["cache"],
+          mod_dir        => "/tmp/junk",
+         }'
+      end
+
+      let(:params) {{ 'default_cache_enable' => true}}
+
+      it { is_expected.to compile }
+      it { is_expected.to contain_class('apache::mod::disk_cache') }
+      it { is_expected.to contain_class('apache::mod::cache').that_comes_before('Class[Apache::Mod::Disk_cache]') }
+      it { is_expected.to contain_apache_mod('cache_disk') }
+      it {
+        is_expected.to contain_file('disk_cache.conf')
+          .with(content: %r{CacheEnable disk \/\nCacheRoot \"\/var\/cache\/apache2\/mod_cache_disk\"\nCacheDirLevels 2\nCacheDirLength 1\nCacheIgnoreHeaders Set-Cookie})
+      }
+    end
+    context 'with $default_cache_enable = foo' do
+      let :pre_condition do
+        'class{ "apache":
+          apache_version => "2.4",
+          default_mods   => ["cache"],
+          mod_dir        => "/tmp/junk",
+         }'
+      end
+
+      let(:params) {{ 'default_cache_enable' => 'foo'}}
+
+      it { is_expected.to compile }
+      it { is_expected.to contain_class('apache::mod::disk_cache') }
+      it { is_expected.to contain_class('apache::mod::cache').that_comes_before('Class[Apache::Mod::Disk_cache]') }
+      it { is_expected.to contain_apache_mod('cache_disk') }
+      it {
+        is_expected.to contain_file('disk_cache.conf')
+          .with(content: %r{CacheEnable disk \/\nCacheRoot \"\/var\/cache\/apache2\/mod_cache_disk\"\nCacheDirLevels 2\nCacheDirLength 1\nCacheIgnoreHeaders Set-Cookie})
+      }.to raise_error(Puppet::Error, /default_cache_enable must be true or false/)
     end
   end
 end
