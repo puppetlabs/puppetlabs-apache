3a83b72d64521468f599dceffc435c35502ca178
Brock Shelton <goabshelto@gmail.com>
2021-06-22 21:38:18 -0400

    Update authz_host.pp

--- manifests/mod/authz_host.pp
+++ manifests/mod/authz_host.pp
@@ -1,9 +1,11 @@
 # @summary
-#   Installs `mod_authnz_host`.
+#   Installs `mod_authz_host`.
 # 
 # @see https://httpd.apache.org/docs/current/mod/mod_authz_host.html for additional documentation.
 #
 class apache::mod::authz_host {
-  include apache
-  ::apache::mod { 'authz_host': }
+  if versioncmp($apache_version, '2.4') >= 0 {
+    include apache
+    ::apache::mod { 'authz_host': }
+  } 
 }
