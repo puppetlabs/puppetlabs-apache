6cfd4e2cc26254dc0f2627cd45fc24c6a2046e77
sheenaajay <sheena@puppet.com>
2021-07-22 12:51:39 +0100

    Update spec/spec_helper_acceptance_local.rb
    Co-authored-by: Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>

--- spec/spec_helper_acceptance_local.rb
+++ spec/spec_helper_acceptance_local.rb
@@ -38,7 +38,7 @@ RSpec.configure do |c|
 
     LitmusHelper.instance.run_shell('puppet module install stahnma/epel')
 
-    LitmusHelper.instance.run_shell('yum-config-manager --disable rhui-rhel-7-server-rhui-debug-rpms') if os[:family] == 'redhat' && os[:release].to_f == 7
+    LitmusHelper.instance.run_shell('yum-config-manager --disable rhui-rhel-7-server-rhui-debug-rpms') if os[:family] == 'redhat' && os[:release].to_i == 7
     pp = <<-PUPPETCODE
     # needed by tests
     package { 'curl':
