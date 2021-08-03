55225f0aa1fa01202dba9cb643ed029c1c721e5d
sheena <sheena@puppet.com>
2021-08-03 11:03:36 +0100

    (maint)

--- spec/classes/mod/proxy_wstunnel.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require 'spec_helper'
-
-describe 'apache::mod::proxy_wstunnel', type: :class do
-  it_behaves_like 'a mod class, without including apache'
-end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -46,6 +46,7 @@ RSpec.configure do |c|
   end
   c.filter_run_excluding(bolt: true) unless ENV['GEM_BOLT']
   c.after(:suite) do
+    RSpec::Puppet::Coverage.report!(0)
   end
 
   # Filter backtrace noise
