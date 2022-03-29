a88bd78ac74bb09292032ea993c3bdafafc51afd
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2022-03-29 15:30:19 +0200

    Drop Apache 2.0 compatibility code
    It's safe to assume the version is >= 2.2 so the conditional can be
    dropped.

--- manifests/mod/proxy_connect.pp
+++ manifests/mod/proxy_connect.pp
@@ -1,18 +1,9 @@
 # @summary
 #   Installs `mod_proxy_connect`.
-# 
-# @param apache_version
-#   Used to verify that the Apache version you have requested is compatible with the module.
-# 
 # @see https://httpd.apache.org/docs/current/mod/mod_proxy_connect.html for additional documentation.
 #
-class apache::mod::proxy_connect (
-  $apache_version  = undef,
-) {
+class apache::mod::proxy_connect {
   include apache
-  $_apache_version = pick($apache_version, $apache::apache_version)
-  if versioncmp($_apache_version, '2.2') >= 0 {
-    Class['::apache::mod::proxy'] -> Class['::apache::mod::proxy_connect']
-    ::apache::mod { 'proxy_connect': }
-  }
+  Class['apache::mod::proxy'] -> Class['apache::mod::proxy_connect']
+  apache::mod { 'proxy_connect': }
 }
