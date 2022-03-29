86c852c2804e016e0adfc59eff4f0bbb8d886f43
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2022-03-29 15:30:19 +0200

    Drop Apache 2.0 compatibility code
    It's safe to assume the version is >= 2.2 so the conditional can be
    dropped.

--- spec/classes/mod/proxy_connect_spec.rb
+++ spec/classes/mod/proxy_connect_spec.rb
@@ -11,31 +11,5 @@ describe 'apache::mod::proxy_connect', type: :class do
 
   include_examples 'a mod class, without including apache'
 
-  context 'with Apache version < 2.2' do
-    let :params do
-      {
-        apache_version: '2.1',
-      }
-    end
-
-    it { is_expected.not_to contain_apache__mod('proxy_connect') }
-  end
-  context 'with Apache version = 2.2' do
-    let :params do
-      {
-        apache_version: '2.2',
-      }
-    end
-
-    it { is_expected.to contain_apache__mod('proxy_connect') }
-  end
-  context 'with Apache version >= 2.4' do
-    let :params do
-      {
-        apache_version: '2.4',
-      }
-    end
-
-    it { is_expected.to contain_apache__mod('proxy_connect') }
-  end
+  it { is_expected.to contain_apache__mod('proxy_connect') }
 end
