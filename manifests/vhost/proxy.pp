c3b91c7a8bdbb6a8f0b26c2367e951c61396b2b3
William Bradford Clark <wclark@redhat.com>
2022-05-27 13:51:15 -0400

    Document reverse proxy resource and data types.
    Adds thorough documentation for the Apache::Vhost::Proxy
    resource type and Apache::Vhost::ProxyPass data type introduced
    by the commit b37682. Addresses issues with clarity and
    coverage identified in code review.

--- manifests/vhost/proxy.pp
+++ manifests/vhost/proxy.pp
@@ -1,14 +1,90 @@
-# @summary Configure a reverse reverse proxy for a vhost
+# @summary Configure a reverse proxy for a vhost
+#
+# @param vhost
+#   The title of the vhost resource to which reverse proxy configuration will
+#   be appended.
+#
+# @param priority
+#   Set the priority to match the one `apache::vhost` sets. This must match the
+#   one `apache::vhost` sets or else the vhost's `concat` resource won't be found.
+#
+# @param order
+#   The order in which the `concat::fragment` containing the proxy configuration
+#   will be inserted. Useful when multiple fragments will be attached to a single
+#   vhost's configuration.
+#
+# @param port
+#   Set the port to match the one `apache::vhost` sets. This must match the one
+#   `apache::vhost` sets or else the vhost's `concat` resource won't be found.
 #
 # @param proxy_dest
-#   Specifies the destination address of a [ProxyPass](https://httpd.apache.org/docs/current/mod/mod_proxy.html#proxypass) configuration.
+#   Specifies the destination address of a [ProxyPass](https://httpd.apache.org/docs/current/mod/mod_proxy.html#proxypass) configuration for the `/` path.
+#
+# @param proxy_dest_match
+#   This directive is equivalent to `proxy_dest`, but takes regular expressions, see
+#   [ProxyPassMatch](https://httpd.apache.org/docs/current/mod/mod_proxy.html#proxypassmatch)
+#   for details.
+#
+# @param proxy_dest_reverse_match
+#   Allows you to pass a ProxyPassReverse if `proxy_dest_match` is specified. See
+#   [ProxyPassReverse](https://httpd.apache.org/docs/current/mod/mod_proxy.html#proxypassreverse)
+#   for details.
+#
+# @param no_proxy_uris
+#   Paths to be excluded from reverse proxying. Only valid when already using `proxy_dest`
+#   or `proxy_dest_match` to map the `/` path, otherwise it will be absent in the final
+#   vhost configuration file. In that case, instead add `no_proxy_uris => [uri1, uri2, ...]`
+#   to the `Apache::Vhost::ProxyPass` definitions passed via the `proxy_pass` parameter.
+#   See examples for this class, or refer to documentation for the `Apache::Vhost::ProxyPass`
+#   data type. This configuration uses the [ProxyPass](https://httpd.apache.org/docs/current/mod/mod_proxy.html#proxypass) directive with `!`.
+#
+# @param no_proxy_uris_match
+#   This directive is equivalent to `no_proxy_uris` but takes regular expressions,
+#   as it instead uses [ProxyPassMatch](https://httpd.apache.org/docs/current/mod/mod_proxy.html#proxypassmatch).
 #
 # @param proxy_pass
 #   Specifies an array of `path => URI` values for a [ProxyPass](https://httpd.apache.org/docs/current/mod/mod_proxy.html#proxypass)
-#   configuration. Optionally, parameters can be added as an array.
-#   ``` puppet
-#   apache::vhost { 'site.name.fdqn':
-#     …
+#   configuration.
+#   See the documentation for the Apache::Vhost::ProxyPass data type for a detailed
+#   description of the structure including optional parameters.
+#
+# @param proxy_pass_match
+#   This directive is equivalent to `proxy_pass`, but takes regular expressions, see
+#   [ProxyPassMatch](https://httpd.apache.org/docs/current/mod/mod_proxy.html#proxypassmatch)
+#   for details.
+#
+# @param proxy_requests
+#   Enables forward (standard) proxy requests. See [ProxyRequests](https://httpd.apache.org/docs/current/mod/mod_proxy.html#proxyrequests) for details.
+#
+# @param proxy_preserve_host
+#   When enabled, pass the `Host:` line from the incoming request to the proxied host.
+#   See [ProxyPreserveHost](https://httpd.apache.org/docs/current/mod/mod_proxy.html#proxypreservehost) for details.
+#
+# @param proxy_add_headers
+#   Add X-Forwarded-For, X-Forwarded-Host, and X-Forwarded-Server HTTP headers.
+#   See [ProxyAddHeaders](https://httpd.apache.org/docs/current/mod/mod_proxy.html#proxyaddheaders) for details.
+#
+# @param proxy_error_override
+#   Override error pages from the proxied host. The current Puppet implementation
+#   supports enabling or disabling the directive, but not specifying a custom list
+#   of status codes. See [ProxyErrorOverride](https://httpd.apache.org/docs/current/mod/mod_proxy.html#proxyerroroverride) for details.
+#
+# @example Simple configuration proxying "/" but not "/admin"
+#   include apache
+#   apache::vhost { 'basic-proxy-vhost':
+#   }
+#   apache::vhost::proxy { 'proxy-to-backend-server':
+#     vhost => 'basic-proxy-vhost',
+#     proxy_dest => 'http://backend-server/',
+#     no_proxy_uris => '/admin',
+#   }
+#
+# @example Granular configuration using `Apache::Vhost::ProxyPass` data type
+#   include apache
+#   apache::vhost { 'myvhost':
+#   }
+#   apache::vhost::proxy { 'myvhost-proxy':
+#     vhost      => 'myvhost',
 #     proxy_pass => [
 #       { 'path' => '/a', 'url' => 'http://backend-a/' },
 #       { 'path' => '/b', 'url' => 'http://backend-b/' },
@@ -27,42 +103,23 @@
 #         'no_proxy_uris' => ['/h/admin', '/h/server-status'] },
 #     ],
 #   }
-#   ```
-#   * `reverse_urls`. *Optional.* This setting is useful when used with `mod_proxy_balancer`. Values: an array or string.
-#   * `reverse_cookies`. *Optional.* Sets `ProxyPassReverseCookiePath` and `ProxyPassReverseCookieDomain`.
-#   * `params`. *Optional.* Allows for ProxyPass key-value parameters, such as connection settings.
-#   * `setenv`. *Optional.* Sets [environment variables](https://httpd.apache.org/docs/current/mod/mod_proxy.html#envsettings) for the proxy directive. Values: array.
 #
-# @param proxy_dest_match
-#   This directive is equivalent to `proxy_dest`, but takes regular expressions, see
-#   [ProxyPassMatch](https://httpd.apache.org/docs/current/mod/mod_proxy.html#proxypassmatch)
-#   for details.
-#
-# @param proxy_dest_reverse_match
-#   Allows you to pass a ProxyPassReverse if `proxy_dest_match` is specified. See
-#   [ProxyPassReverse](https://httpd.apache.org/docs/current/mod/mod_proxy.html#proxypassreverse)
-#   for details.
-#
-# @param proxy_pass_match
-#   This directive is equivalent to `proxy_pass`, but takes regular expressions, see
-#   [ProxyPassMatch](https://httpd.apache.org/docs/current/mod/mod_proxy.html#proxypassmatch)
-#   for details.
 define apache::vhost::proxy(
   String[1] $vhost,
   $priority = undef,
   Integer[0] $order = 170,
   Optional[Stdlib::Port] $port = undef,
   Optional[String[1]] $proxy_dest = undef,
-  Optional[Array[Apache::Vhost::ProxyPass]] $proxy_pass = undef,
-  Optional[Array[Apache::Vhost::ProxyPass]] $proxy_pass_match = undef,
   Optional[String[1]] $proxy_dest_match = undef,
   Optional[String[1]] $proxy_dest_reverse_match = undef,
+  Variant[Array[String[1]], String[1]] $no_proxy_uris = [],
+  Variant[Array[String[1]], String[1]] $no_proxy_uris_match = [],
+  Optional[Array[Apache::Vhost::ProxyPass]] $proxy_pass = undef,
+  Optional[Array[Apache::Vhost::ProxyPass]] $proxy_pass_match = undef,
   Boolean $proxy_requests = false,
   Boolean $proxy_preserve_host = false,
   Optional[Boolean] $proxy_add_headers = undef,
   Boolean $proxy_error_override = false,
-  Variant[Array[String[1]], String[1]] $no_proxy_uris = [],
-  Variant[Array[String[1]], String[1]] $no_proxy_uris_match = [],
 ) {
   include apache::mod::proxy
   include apache::mod::proxy_http
--- types/vhost/proxypass.pp
+++ types/vhost/proxypass.pp
@@ -1,4 +1,80 @@
-# @summary A proxy pass definition for in an Apache vhost
+# @summary Struct representing reverse proxy configuration for an Apache vhost, used by the Apache::Vhost::Proxy defined resource type.
+#
+# @param [String[1]] path
+#   The virtual path on the local server to map.
+#
+# @param [String[1]] url
+#   The URL to which the path and its children will be mapped.
+#
+# @param [Optional[Hash]] params
+#   Optional [ProxyPass](https://httpd.apache.org/docs/current/mod/mod_proxy.html#proxypass) key-value parameters, such as connection settings.
+#
+# @param [Array[String[1]]] reverse_urls
+#   Optional (but usually recommended) URLs for [ProxyPassReverse](https://httpd.apache.org/docs/current/mod/mod_proxy.html#proxypassreverse) configuration.
+#   Allows Apache to adjust certain headers on HTTP redirect responses, to prevent
+#   redirects on the back-end from bypassing the reverse proxy.
+#
+# @param [Optional[Array[Hash]]] reverse_cookies
+#   Optional Array of Hashes, each representing a ProxyPassReverseCookieDomain or
+#   ProxyPassReverseCookiePath configuration. These are similar to ProxyPassReverse but
+#   operate on Domain or Path strings respectively in Set-Cookie headers. Each Hash
+#   must contain one `url => value` pair, and exactly one `path => value` or `domain => value`
+#   pair, representing the internal domain or internal path.
+# @option reverse_cookies [String[1]] :url
+#   Required partial URL representing public domain or public path.
+# @option reverse_cookies [Optional[String[1]]] :path
+#   Internal path for [ProxyPassReverseCookiePath](https://httpd.apache.org/docs/current/mod/mod_proxy.html#proxypassreversecookiepath) configuration.
+# @option reverse_cookies [Optional[String[1]]] :domain
+#   Internal domain for [ProxyPassReverseCookieDomain](https://httpd.apache.org/docs/current/mod/mod_proxy.html#proxypassreversecookiedomain) configuration.
+#
+# @param [Optional[Array[String[1]]]] keywords
+#   Array of optional keywords such as `nocanon`, `interpolate`, or `noquery` supported
+#   by [ProxyPass](https://httpd.apache.org/docs/current/mod/mod_proxy.html#proxypass) (refer
+#   to subsection under heading "Additional ProxyPass Keywords").
+#
+# @param [Optional[Array[String[1]]]] setenv
+#   Optional Array of Strings of the form "${env_var_name} ${value}".
+#   Uses [SetEnv](https://httpd.apache.org/docs/current/mod/mod_env.html#setenv) to make [Protocol Adjustments](https://httpd.apache.org/docs/current/mod/mod_proxy.html#envsettings) to mod_proxy in the virtual
+# host context. Because the implementation uses SetEnv, you must `include apache::mod::env`;
+# for the same reason, this cannot set the newer `no-proxy` environment variable, which
+# would require an implementation using SetEnvIf.
+#
+# @param [Optional[Array[String[1]]]] no_proxy_uris
+#   Optional Array of paths to exclude from proxying, using the `!` directive to [ProxyPass](https://httpd.apache.org/docs/current/mod/mod_proxy.html#proxypass).
+#
+# @param [Optional[Array[String[1]]]] no_proxy_uris_match
+#   Similar to `no_proxy_uris` but uses [ProxyPassMatch](https://httpd.apache.org/docs/current/mod/mod_proxy.html#proxypassmatch) to allow regular
+#   expressions.
+#
+# @example Basic example
+#   { 'path' => '/a', 'url' => 'http://backend-a/', }
+#
+# @example With parameters
+#   { 'path' => '/b', 'url' => 'http://backend-a/b',
+#     'params' => {'max'=>20, 'ttl'=>120, 'retry'=>300,}, }
+#
+# @example With ProxyPassReverse
+#   { 'path' => '/l', 'url' => 'http://backend-xy',
+#     'reverse_urls' => ['http://backend-x', 'http://backend-y'], }
+#
+# @example With additional keywords
+#   { 'path' => '/e', 'url' => 'http://backend-a/e',
+#     'keywords' => ['nocanon', 'interpolate'], }
+#
+# @example With mod_proxy environment variables
+#   { 'path' => '/f', 'url' => 'http://backend-f/',
+#     'setenv' => ['proxy-nokeepalive 1','force-proxy-request-1.0 1'], }
+#
+# @example With ProxyPassReverseCookieDomain and ProxyPassReverseCookiePath
+#   { 'path' => '/g', 'url' => 'http://backend-g/',
+#     'reverse_cookies' => [{'path' => '/g', 'url' => 'http://backend-g/',}, {'domain' => 'http://backend-g', 'url' => 'http:://backend-g',}], }
+#
+# @example With exclusions
+#   { 'path' => '/h', 'url' => 'http://backend-h/h',
+#     'no_proxy_uris' => ['/h/admin', '/h/server-status'], }
+#
+# @see https://httpd.apache.org/docs/current/mod/mod_proxy.html for additional documentation.
+#
 type Apache::Vhost::ProxyPass = Struct[{
   path                          => String[1],
   url                           => String[1],
