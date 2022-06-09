f8c944904710166f0a2cd19f85082f39c8ff4a70
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2022-06-09 13:04:08 +0200

    Acceptance tests: Stop using EPEL 7 on EL8
    Fixes: cd81ee3c04ee419731483a0b23a59de6659e3382

--- spec/spec_helper_acceptance_local.rb
+++ spec/spec_helper_acceptance_local.rb
@@ -63,12 +63,6 @@ RSpec.configure do |c|
           epel_baseurl => "http://osmirror.delivery.puppetlabs.net/epel${::operatingsystemmajrelease}-\\$basearch/RPMS.all",
           epel_mirrorlist => "http://osmirror.delivery.puppetlabs.net/epel${::operatingsystemmajrelease}-\\$basearch/RPMS.all",
         }
-      } elsif $::operatingsystemmajrelease == '8' {
-          class { 'epel':
-                os_maj_release => "7",
-                epel_baseurl => "http://osmirror.delivery.puppetlabs.net/epel7-\\$basearch/RPMS.all",
-                epel_mirrorlist => "http://osmirror.delivery.puppetlabs.net/epel7-\\$basearch/RPMS.all",
-          }
       } else {
         class { 'epel': }
       }
