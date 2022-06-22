3b6c262066f180147189961f6f8de3893942dc11
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2022-06-22 11:51:52 +0200

    Clean up inclusion of mod::env and mod::setenvif
    There is no need to check if the variables are true because the data
    type doesn't allow anything that evaluates to false. The logic to
    include it is also moved to the template which makes it easier to
    follow the logic.
    
    The same is applied to the template.

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -2190,24 +2190,6 @@ define apache::vhost (
     }
   }
 
-  # Check if mod_env is required and not yet loaded.
-  # create an expression to simplify the conditional check
-  $use_env_mod = $setenv and ! empty($setenv)
-  if ($use_env_mod) {
-    if ! defined(Class['apache::mod::env']) {
-      include apache::mod::env
-    }
-  }
-  # Check if mod_setenvif is required and not yet loaded.
-  # create an expression to simplify the conditional check
-  $use_setenvif_mod = ($setenvif and ! empty($setenvif)) or ($setenvifnocase and ! empty($setenvifnocase))
-
-  if ($use_setenvif_mod) {
-    if ! defined(Class['apache::mod::setenvif']) {
-      include apache::mod::setenvif
-    }
-  }
-
   ## Create a default directory list if none defined
   if $directories {
     $_directories = $directories
@@ -2580,7 +2562,16 @@ define apache::vhost (
   # Template uses:
   # - $setenv
   # - $setenvif
-  if ($use_env_mod or $use_setenvif_mod) {
+  $use_env_mod = !empty($setenv)
+  $use_setenvif_mod = !empty($setenvif) or !empty($setenvifnocase)
+  if ($use_env_mod or $use_setenvif_mod) and $ensure == 'present' {
+    if $use_env_mod {
+      include apache::mod::env
+    }
+    if $use_setenvif_mod {
+      include apache::mod::setenvif
+    }
+
     concat::fragment { "${name}-setenv":
       target  => "${priority_real}${filename}.conf",
       order   => 220,
