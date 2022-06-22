11d37f4fcec19ac605dd2fcb152c1535d59d5709
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2022-06-22 11:37:46 +0200

    Use a stricter data type on apache::vhost::aliases
    This also implements a real test on the rendered template. It appears it
    was passing in something invalid ever since it was introduced in
    f1d64a0a0b71af0102d10da69e333aa9ac5a15f5.

--- spec/defines/vhost_spec.rb
+++ spec/defines/vhost_spec.rb
@@ -109,7 +109,12 @@ describe 'apache::vhost', type: :define do
               'access_log_syslog'           => true,
               'access_log_format'           => '%h %l %u %t \"%r\" %>s %b',
               'access_log_env_var'          => '',
-              'aliases'                     => '/image',
+              'aliases'                     => [
+                {
+                  'alias' => '/image',
+                  'path'  => '/rspec/image',
+                },
+              ],
               'directories'                 => [
                 {
                   'path'     => '/var/www/files',
@@ -619,7 +624,11 @@ describe 'apache::vhost', type: :define do
             )
           }
           it { is_expected.to contain_concat__fragment('rspec.example.com-docroot') }
-          it { is_expected.to contain_concat__fragment('rspec.example.com-aliases') }
+          it {
+            is_expected.to contain_concat__fragment('rspec.example.com-aliases').with(
+              content: %r{^\s+Alias /image "/rspec/image"$},
+            )
+          }
           it { is_expected.to contain_concat__fragment('rspec.example.com-itk') }
           it { is_expected.to contain_concat__fragment('rspec.example.com-fallbackresource') }
           it { is_expected.to contain_concat__fragment('rspec.example.com-directories') }
