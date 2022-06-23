puppetlabs/puppetlabs-apache

commit 2349ba7f35442493d29a08676382044aebaa6eae

(maint) Allow using mod_md for managing certificates
Add a $mdomain parameter to apache::vhost.  When set to true, the
certifcate configuration is automatically managed by mod_md.  It is also
possible to use an explicit String to fully control the Subject
Alternative Names of the requested certificate.

manifests/vhost.pp
@@ -1722,6 +1722,10 @@
 #   value of the $servername parameter.
 #   When set to false (default), the existing behaviour of using the $name parameter
 #   will remain.
+#
+# @param $mdomain
+#   All the names in the list are managed as one Managed Domain (MD). mod_md will request
+#   one single certificate that is valid for all these names.
 
 define apache::vhost (
   Variant[Boolean,String] $docroot,
@@ -1971,6 +1975,7 @@ define apache::vhost (
   Hash $define                                                                      = {},
   Boolean $auth_oidc                                                                = false,
   Optional[Apache::OIDCSettings] $oidc_settings                                     = undef,
+  Optional[Variant[Boolean,String]] $mdomain                                        = undef,
 ) {
   # The base class must be included first because it is used by parameter defaults
   if ! defined(Class['apache']) {
@@ -2769,6 +2774,10 @@ define apache::vhost (
       order   => 300,
       content => template('apache/vhost/_http2.erb'),
     }
+  }
+
+  if $mdomain {
+    include apache::mod::md
   }
 
   # Template uses:

spec/defines/vhost_spec.rb
@@ -484,6 +484,7 @@ describe 'apache::vhost', type: :define do
                                                  'RemoteUserClaim'           => 'sub',
                                                  'ClientSecret'              => 'aae053a9-4abf-4824-8956-e94b2af335c8',
                                                  'CryptoPassphrase'          => '4ad1bb46-9979-450e-ae58-c696967df3cd' },
+              'mdomain'                     => 'example.com example.net auto',
             }
           end
 
@@ -1483,6 +1484,12 @@ describe 'apache::vhost', type: :define do
               content: %r{^\s+OIDCCryptoPassphrase\s4ad1bb46-9979-450e-ae58-c696967df3cd$},
             )
           }
+          it { is_expected.to contain_class('apache::mod::md') }
+          it {
+            is_expected.to contain_concat__fragment('rspec.example.com-apache-header').with(
+              content: %r{^MDomain example\.com example\.net auto$},
+            )
+          }
         end
         context 'vhost with multiple ip addresses' do
           let :params do
@@ -2450,6 +2457,19 @@ describe 'apache::vhost', type: :define do
 
             it { is_expected.not_to compile }
           end
+        end
+        context 'mdomain' do
+          let :params do
+            default_params.merge(
+              'mdomain' => true,
+            )
+          end
+
+          it {
+            is_expected.to contain_concat__fragment('rspec.example.com-apache-header').with(
+              content: %r{^MDomain rspec.example.com$},
+            )
+          }
         end
       end
     end

templates/vhost/_file_header.erb
@@ -3,6 +3,14 @@
 # Managed by Puppet
 # ************************************
 <%= [@comment].flatten.collect{|c| "# #{c}"}.join("\n") -%>
+<% if @mdomain -%>
+
+  <%- if @mdomain.is_a?(String) -%>
+MDomain <%= @mdomain %>
+  <%- else -%>
+MDomain <%= @servername %>
+  <%- end -%>
+<% end -%>
 
 <VirtualHost <%= [@nvh_addr_port].flatten.compact.join(' ') %>>
 <% @define.each do | k, v| -%>

templates/vhost/_ssl.erb
@@ -2,8 +2,10 @@
 
   ## SSL directives
   SSLEngine on
+  <%- unless @mdomain -%>
   SSLCertificateFile      "<%= @ssl_cert %>"
   SSLCertificateKeyFile   "<%= @ssl_key %>"
+  <%- end -%>
   <%- if @ssl_chain -%>
   SSLCertificateChainFile "<%= @ssl_chain %>"
   <%- end -%>

commit 0c5c847f27f9cf41a91ff5298b24d7cad007df7d

Document uses of $mdomain
Fixes: 2349ba7f35442493d29a08676382044aebaa6eae

manifests/vhost.pp
@@ -2281,6 +2281,7 @@ define apache::vhost (
   # - $protocols
   # - $protocols_honor_order
   # - $apache_version
+  # - $mdomain
   concat::fragment { "${name}-apache-header":
     target  => "${priority_real}${filename}.conf",
     order   => 0,
@@ -2598,6 +2599,7 @@ define apache::vhost (
   # - $ssl_openssl_conf_cmd
   # - $ssl_stapling
   # - $apache_version
+  # - $mdomain
   if $ssl and $ensure == 'present' {
     include apache::mod::ssl
 