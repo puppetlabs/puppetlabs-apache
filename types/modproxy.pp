978c1efa9cddb1122bce8e14a8a61e6805e5f012
canth1 <71357932+canth1@users.noreply.github.com>
2022-08-15 14:11:48 -0600

    Create modproxy.pp

---
+++ types/modproxy.pp
@@ -0,0 +1,13 @@
+# @summary Supported protocols / schemes by mod_proxy
+# @see https://httpd.apache.org/docs/2.4/mod/mod_proxy.html
+type Apache:ModProxyProtocol = Pattern[
+    /(?i:\Aajp:\/\/.*\z)/,
+    /(?i:\Afcgi:\/\/.*\z)/,
+    /(?i:\Aftp:\/\/.*\z)/,
+    /(?i:\Ah2c?:\/\/.*\z)/,
+    /(?i:\Ahttps?:\/\/.*\z)/,
+    /(?i:\Ascgi:\/\/.*\z)/,
+    /(?i:\Aunix:\/([^\n\/\0]+\/*)*\z)/
+    /(?i:\Auwsgi:\/\/.*\z)/,
+    /(?i:\Awss?:\/\/.*\z)/,
+ ]
