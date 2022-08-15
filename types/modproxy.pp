83054c1c3067745a575c2bb43d74d4d2b7738e2d
canth1 <71357932+canth1@users.noreply.github.com>
2022-08-15 14:12:12 -0600

    Update modproxy.pp

--- types/modproxy.pp
+++ types/modproxy.pp
@@ -1,6 +1,6 @@
 # @summary Supported protocols / schemes by mod_proxy
 # @see https://httpd.apache.org/docs/2.4/mod/mod_proxy.html
-type Apache:ModProxyProtocol = Pattern[
+type Apache::ModProxyProtocol = Pattern[
     /(?i:\Aajp:\/\/.*\z)/,
     /(?i:\Afcgi:\/\/.*\z)/,
     /(?i:\Aftp:\/\/.*\z)/,
