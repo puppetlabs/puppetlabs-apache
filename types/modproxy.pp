36f238a31fa481266e62bad318c4b65c1d001709
canth1 <71357932+canth1@users.noreply.github.com>
2022-08-17 10:20:00 -0600

    Update modproxy.pp

--- types/modproxy.pp
+++ types/modproxy.pp
@@ -1,3 +1,3 @@
 # @summary Supported protocols / schemes by mod_proxy
 # @see https://httpd.apache.org/docs/2.4/mod/mod_proxy.html
-type Apache::ModProxyProtocol = Pattern[/(\A(ajp|fcgi|ftp|h2|h2c|http|https|scgi|uwsgi|ws|wss):\/\/.*\z)/, /(\Aunix:\/([^\n\/\0]+\/*)*\z)/]
+type Apache::ModProxyProtocol = Pattern[/(\A(ajp|fcgi|ftp|h2c?|https?|scgi|uwsgi|wss?):\/\/.*\z)/, /(\Aunix:\/([^\n\/\0]+\/*)*\z)/]
