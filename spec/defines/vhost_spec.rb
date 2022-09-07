9671f3d859e62764c63cdb9c91846ba3ef71aadf
Martin Körner <martin.koerner@gmail.com>
2022-09-07 20:01:53 +0200

    Add test for rewrite_inherit with empty rewrites

--- spec/defines/vhost_spec.rb
+++ spec/defines/vhost_spec.rb
@@ -1690,10 +1690,33 @@ describe 'apache::vhost', type: :define do
             it { is_expected.to raise_error(Puppet::Error) }
           end
           context 'empty rewrites' do
-            let(:params) { super().merge('rewrites' => []) }
+            let(:params) {
+              super().merge(
+                'rewrite_inherit' => false,
+                'rewrites' => []
+              )
+            }
 
-            it { is_expected.to compile }
+            it { 
+              is_expected.to compile
+              is_expected.not_to contain_concat__fragment('rspec.example.com-rewrite')
+            }            
           end
+          context 'empty rewrites_with_rewrite_inherit' do
+            let(:params) {
+              super().merge(
+                'rewrite_inherit' => true,
+                'rewrites' => []
+              )
+            }
+  
+            it {
+              is_expected.to contain_concat__fragment('rspec.example.com-rewrite').with(
+                content: %r{^\s+RewriteOptions Inherit$},
+              )
+            }
+          end
+
           context 'bad error_log_format flag' do
             let :params do
               super().merge(
