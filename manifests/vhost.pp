cd935a594b6cc2495f382851651c14f3b9545a25
Tim Meusel <tim@bastelfreak.de>
2023-01-10 13:28:33 +0100

    vhost: Make ProxyAddHeaders configureable
    It's a valid usecase to set ProxyAddHeaders to Off. That's currently
    only possible when other proxy settings are configured as well. It needs
    to be independant.

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -2448,7 +2448,7 @@ define apache::vhost (
   # - $proxy_preserve_host
   # - $proxy_add_headers
   # - $no_proxy_uris
-  if ($proxy_dest or $proxy_pass or $proxy_pass_match or $proxy_dest_match or $proxy_preserve_host) and $ensure == 'present' {
+  if ($proxy_dest or $proxy_pass or $proxy_pass_match or $proxy_dest_match or $proxy_preserve_host or ($proxy_add_headers =~ NotUndef)) and $ensure == 'present' {
     include apache::mod::proxy_http
 
     concat::fragment { "${name}-proxy":
