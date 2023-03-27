3ea777f0c463768abd85c2b4ca8e3de17083a64c
Tames McTigue <tames@northwestern.edu>
2023-03-27 22:17:13 +0300

    Updated modsec_link_spec with updated data.

--- spec/defines/modsec_link_spec.rb
+++ spec/defines/modsec_link_spec.rb
@@ -22,12 +22,21 @@ describe 'apache::security::rule_link', type: :define do
       it { is_expected.to compile.with_all_deps }
       case facts[:os]['family']
       when 'RedHat'
-        it {
-          is_expected.to contain_file('modsecurity_35_bad_robots.data').with(
-            path: '/etc/httpd/modsecurity.d/activated_rules/modsecurity_35_bad_robots.data',
-            target: '/usr/lib/modsecurity.d/base_rules/modsecurity_35_bad_robots.data',
-          )
-        }
+        if facts[:os]['release']['major'].to_i <= 7
+          it {
+            is_expected.to contain_file('modsecurity_35_bad_robots.data').with(
+              path: '/etc/httpd/modsecurity.d/activated_rules/modsecurity_35_bad_robots.data',
+              target: '/usr/lib/modsecurity.d/base_rules/modsecurity_35_bad_robots.data',
+            )
+          }
+        else
+          it {
+            is_expected.to contain_file('modsecurity_35_bad_robots.data').with(
+              path: '/etc/httpd/modsecurity.d/activated_rules/modsecurity_35_bad_robots.data',
+              target: '/usr/share/mod_modsecurity_crs/base_rules/modsecurity_35_bad_robots.data',
+            )
+          }
+        end
       when 'Debian'
         it {
           is_expected.to contain_file('modsecurity_35_bad_robots.data').with(
