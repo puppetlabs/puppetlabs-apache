462a98eb38f52d17af3e415810ce401d8dcba677
Lukas Audzevicius <laudzevicius24@hotmail.com>
2023-04-21 11:02:04 +0100

    (CONT-772) Manual correct Style/MixinUsage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,7 +9,7 @@ require 'rspec-puppet-facts'
 
 require 'spec_helper_local' if File.file?(File.join(File.dirname(__FILE__), 'spec_helper_local.rb'))
 
-include RspecPuppetFacts
+include RspecPuppetFacts # rubocop:disable Style/MixinUsage
 
 default_facts = {
   puppetversion: Puppet.version,
