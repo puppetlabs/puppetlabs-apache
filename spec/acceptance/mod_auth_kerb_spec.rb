1a168dcdac1cd1e86e6fc3a5149bd8c8d3eacdad
Dinesh Gujar <dinesh.gujar@Dineshs-MacBook-Pro.local>
2023-09-07 07:00:07 +0530

    CAT-1284 acceptance added

---
+++ spec/acceptance/mod_auth_kerb_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'spec_helper_acceptance'
+apache_hash = apache_settings_hash
+describe 'apache::vhost define auth kerb' do
+
+  context 'new vhost on port 80' do
+    pp = <<-MANIFEST
+      class { 'apache': }
+      file { '/var/www':
+        ensure  => 'directory',
+        recurse => true,
+      }
+
+      apache::vhost { 'first.example.com':
+        port    => 80,
+        docroot => '/var/www/first',
+        require => File['/var/www'],
+        auth_kerb => true,
+      }
+    MANIFEST
+    it 'configures an apache vhost' do
+      apply_manifest(pp, catch_failures: true)
+    end
+
+    describe file("#{apache_hash['vhost_dir']}/25-first.example.com.conf") do
+      it { is_expected.to contain '<VirtualHost \*:80>' }
+      it { is_expected.to contain 'ServerName first.example.com' }
+    end
+  end
+
+
+
+end
