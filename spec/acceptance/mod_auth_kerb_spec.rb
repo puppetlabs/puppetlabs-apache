df1f79b94a3656944bae903ab06694968479c506
Dinesh Gujar <dinesh.gujar@Dineshs-MacBook-Pro.local>
2023-09-07 11:47:29 +0530

    CAT-1284 spec manifest config fix

--- spec/acceptance/mod_auth_kerb_spec.rb
+++ spec/acceptance/mod_auth_kerb_spec.rb
@@ -30,10 +30,5 @@ describe 'apache::vhost define auth kerb' do
     it 'configures an apache vhost' do
       apply_manifest(pp, catch_failures: true)
     end
-
-    describe file("#{apache_hash['vhost_dir']}/25-first.example.com.conf") do
-      it { is_expected.to contain '<VirtualHost \*:80>' }
-      it { is_expected.to contain 'ServerName first.example.com' }
-    end
   end
 end
