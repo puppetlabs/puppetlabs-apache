e31003807dd948af32fa5349fcadac3d28f82770
Dinesh Gujar <dinesh.gujar@Dineshs-MacBook-Pro.local>
2023-09-08 11:56:01 +0530

    susexec and shib converted

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -2706,7 +2706,7 @@ define apache::vhost (
     concat::fragment { "${name}-suexec":
       target  => "${priority_real}${filename}.conf",
       order   => 290,
-      content => epp('apache/vhost/_suexec.epp',{'suexec_user_group'=> $suexec_user_group}),
+      content => epp('apache/vhost/_suexec.epp', { 'suexec_user_group' => $suexec_user_group }),
     }
   }
 
