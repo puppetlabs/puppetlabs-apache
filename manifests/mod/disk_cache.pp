60c3d46fb169829c52ad71e3f9c9969fb01116c6
Dennis Ploeger <dennis.ploeger@kps.com>
2024-01-12 09:58:07 +0100

    Fixing parameters in deprecated module to keep backwards compatibility

--- manifests/mod/disk_cache.pp
+++ manifests/mod/disk_cache.pp
@@ -6,8 +6,6 @@
 #   Default depends on the Apache version and operating system:
 #   - Debian: /var/cache/apache2/mod_cache_disk
 #   - FreeBSD: /var/cache/mod_cache_disk
-#   - Red Hat, Apache 2.4: /var/cache/httpd/proxy
-#   - Red Hat, Apache 2.2: /var/cache/mod_proxy
 #
 # @param cache_ignore_headers
 #   Specifies HTTP header(s) that should not be stored in the cache.
@@ -16,13 +14,12 @@
 #   Apache 2.2, mod_disk_cache installed. On Apache 2.4, mod_cache_disk installed.
 #   This class is deprecated, use mode_cache_disk instead
 #
-# @see https://httpd.apache.org/docs/2.2/mod/mod_disk_cache.html for additional documentation on version 2.2.
-#
 # @see https://httpd.apache.org/docs/2.4/mod/mod_cache_disk.html for additional documentation on version 2.4.
 #
 class apache::mod::disk_cache (
-  $cache_root                 = undef,
-  $cache_ignore_headers       = undef,
+  Optional[Stdlib::Absolutepath] $cache_root = undef,
+  Optional[String] $cache_ignore_headers     = undef,
+  Boolean $default_cache_enable              = true,
 ) {
   deprecation('apache::mod::disk_cache','This module is deprecated; please use apache::mod::cache_disk')
 
