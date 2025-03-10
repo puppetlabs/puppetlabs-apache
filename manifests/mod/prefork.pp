puppetlabs/puppetlabs-apache

commit cedd45b63be89ea54bd2a596e6cd3a3f60d4faf8

Drop Apache 2.2 support

manifests/mod/prefork.pp
@@ -25,9 +25,6 @@
 # @param maxconnectionsperchild
 #   Limit on the number of connections that an individual child server will handle during its life.
 #
-# @param apache_version
-#   Used to verify that the Apache version you have requested is compatible with the module.
-#
 # @param listenbacklog
 #   Maximum length of the queue of pending connections.
 # 
@@ -42,32 +39,17 @@ class apache::mod::prefork (
   Optional[Integer] $maxrequestworkers      = undef,
   Integer $maxrequestsperchild              = 4000,
   Optional[Integer] $maxconnectionsperchild = undef,
-  Optional[String] $apache_version          = undef,
   Integer $listenbacklog                    = 511
 ) {
   include apache
-  $_apache_version = pick($apache_version, $apache::apache_version)
   if defined(Class['apache::mod::event']) {
     fail('May not include both apache::mod::prefork and apache::mod::event on the same node')
-  }
-  if versioncmp($_apache_version, '2.4') < 0 {
-    if defined(Class['apache::mod::itk']) {
-      fail('May not include both apache::mod::prefork and apache::mod::itk on the same node')
-    }
   }
   if defined(Class['apache::mod::peruser']) {
     fail('May not include both apache::mod::prefork and apache::mod::peruser on the same node')
   }
   if defined(Class['apache::mod::worker']) {
     fail('May not include both apache::mod::prefork and apache::mod::worker on the same node')
-  }
-
-  if versioncmp($_apache_version, '2.3.13') < 0 {
-    if $maxrequestworkers == undef {
-      warning("For newer versions of Apache, \$maxclients is deprecated, please use \$maxrequestworkers.")
-    } elsif $maxconnectionsperchild == undef {
-      warning("For newer versions of Apache, \$maxrequestsperchild is deprecated, please use \$maxconnectionsperchild.")
-    }
   }
 
   File {
@@ -94,31 +76,12 @@ class apache::mod::prefork (
   }
 
   case $facts['os']['family'] {
-    'redhat': {
-      if versioncmp($_apache_version, '2.4') >= 0 {
-        ::apache::mpm { 'prefork':
-          apache_version => $_apache_version,
-        }
-      }
-      else {
-        file_line { '/etc/sysconfig/httpd prefork enable':
-          ensure  => present,
-          path    => '/etc/sysconfig/httpd',
-          line    => '#HTTPD=/usr/sbin/httpd.worker',
-          match   => '#?HTTPD=/usr/sbin/httpd.worker',
-          require => Package['httpd'],
-          notify  => Class['apache::service'],
-        }
-      }
-    }
-    'debian', 'freebsd': {
+    'redhat', 'debian', 'freebsd': {
       ::apache::mpm { 'prefork':
-        apache_version => $_apache_version,
       }
     }
     'Suse': {
       ::apache::mpm { 'prefork':
-        apache_version => $apache_version,
         lib_path       => '/usr/lib64/apache2-prefork',
       }
     }

commit 79ba18c523fc53c911cac3e42fd53271a9494301

Correct whitespace in arrow alignment
Fixes: cedd45b63be8 ("Drop Apache 2.2 support")

manifests/mod/prefork.pp
@@ -94,7 +94,7 @@ class apache::mod::prefork (
     }
     'Suse': {
       ::apache::mpm { 'prefork':
-        lib_path       => '/usr/lib64/apache2-prefork',
+        lib_path => '/usr/lib64/apache2-prefork',
       }
     }
     'Gentoo': {